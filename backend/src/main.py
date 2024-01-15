9c77167381b85f40b62ffe6a6df0f99a763bcfd2
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-01-15 16:28:48 +0200

    add concept for ai file communication

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,14 @@
 from engine_wrapper import EngineWrapper
+import time
 
 print("Hello world!! Let's play chess :DDDDDDDDD")
 
 engine_wrapper = EngineWrapper([], 5)
 
-for _ in range(4):
+for _ in range(999):
     best_move = engine_wrapper.calculate_move()
     engine_wrapper.add_moves([best_move])
-
-print(engine_wrapper.boardstate)
+    time.sleep(0.1)
+    print(engine_wrapper.engine.get_board_visual())
+    print(engine_wrapper.boardstate)
+    print(engine_wrapper.engine.get_evaluation())
