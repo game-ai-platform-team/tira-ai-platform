a0a78e9b6076f8e7d87ffbaf0b36fd485beb9d26
MiikaMatias <100348027+MiikaMatias@users.noreply.github.com>
2024-01-15 13:02:01 +0200

    2: implemented a rudimentary stockfish 15 engine into the backend

---
+++ backend/src/engine_wrapper.py
@@ -0,0 +1,28 @@
+from stockfish.models import Stockfish as sf
+
+class EngineWrapper:
+
+    def __init__(self,boardstate: list, depth: int) -> None:
+        self.engine = sf(path="engines\stockfish_15_win_x64_avx2\stockfish_15_x64_avx2.exe")
+        
+        self.depth = depth
+        self.engine.set_depth(depth)
+
+        self.boardstate = boardstate
+        self.engine.set_position(boardstate)
+
+    def set_depth(self, depth: int):
+        self.depth = depth
+
+    def set_board(self, boardstate: list):
+        self.boardstate = boardstate
+        self.engine.set_position(boardstate)
+
+    def add_moves(self,moves: list):
+        for move in moves:
+            self.boardstate.append(move)
+            self.engine.set_position(self.boardstate)
+
+    def calculate_move(self):
+        return self.engine.get_best_move()
+
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,11 @@
+from engine_wrapper import EngineWrapper
 
+print("Hello world!! Let's play chess :DDDDDDDDD")
 
-print("Hello world!!")
+engine_wrapper = EngineWrapper([], 5)
+
+for _ in range(4):
+    best_move = engine_wrapper.calculate_move()
+    engine_wrapper.add_moves([best_move])
+
+print(engine_wrapper.boardstate)
