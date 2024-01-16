6c020539c9151e1c2c04760fe340b12fade929b6
MiikaMatias <miikapiiparinen24@gmail.com>
2024-01-16 10:55:02 +0200

    add pylint

--- backend/src/chess.py
+++ backend/src/chess.py
@@ -1,18 +1,20 @@
-from engine_wrapper import EngineWrapper
 import subprocess
 import time
+from engine_wrapper import EngineWrapper
 
 class Chess:
     def __init__(self, ai_name):
         self.engine_wrapper = EngineWrapper([], 5)
         self.ai_name = ai_name
-    
+
     def get_move_from_ai(self, boardstate):
-        return subprocess.run(["python", f"src/{self.ai_name}", f"-b {','.join(boardstate)}"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    
+        return subprocess.run(
+            ["python", f"src/{self.ai_name}", f"-b {','.join(boardstate)}"],
+            stdout=subprocess.PIPE).stdout.decode('utf-8')
+
     def get_move_from_self(self):
         return self.engine_wrapper.calculate_move()
-        
+
     def play(self, turns, delay):
         self.print_board()
         time.sleep(delay)
@@ -21,11 +23,12 @@ class Chess:
             self_move = self.get_move_from_self()
 
             try:
-                self.engine_wrapper.engine.make_moves_from_current_position([self_move])
-            except:
+                self.engine_wrapper.engine.make_moves_from_current_position([
+                                                                            self_move])
+            except BaseException:
                 print(self.engine_wrapper.engine.get_evaluation())
                 break
-            
+
             self.engine_wrapper.boardstate.append(self_move)
 
             self.print_board()
@@ -34,8 +37,9 @@ class Chess:
             ai_move = self.get_move_from_ai(self.engine_wrapper.boardstate)
 
             try:
-                self.engine_wrapper.engine.make_moves_from_current_position([ai_move])
-            except:
+                self.engine_wrapper.engine.make_moves_from_current_position([
+                                                                            ai_move])
+            except BaseException:
                 print(self.engine_wrapper.engine.get_evaluation())
                 break
 
@@ -52,8 +56,6 @@ class Chess:
         print(self.engine_wrapper.boardstate)
 
 
-
 if __name__ == "__main__":
     c = Chess("test_ai.py")
     c.play(1000, 0.1)
-    
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,22 @@
-from engine_wrapper import EngineWrapper
+"""
+Wraps Stockfish
+"""
+
 import time
+import os
+from engine_wrapper import EngineWrapper
 
-print("Hello world!! Let's play chess :DDDDDDDDD")
 
-engine_wrapper = EngineWrapper([], 5)
+if __name__ == "__main__":
+    print(os.getcwd())
+    print("Hello world!! Let's play chess :DDDDDDDDD")
 
-for _ in range(999):
-    best_move = engine_wrapper.calculate_move()
-    engine_wrapper.add_moves([best_move])
-    time.sleep(0.1)
-    print(engine_wrapper.engine.get_board_visual())
-    print(engine_wrapper.boardstate)
-    print(engine_wrapper.engine.get_evaluation())
+    engine_wrapper = EngineWrapper([], 5)
+
+    for _ in range(999):
+        best_move = engine_wrapper.calculate_move()
+        engine_wrapper.add_moves([best_move])
+        time.sleep(0.1)
+        print(engine_wrapper.engine.get_board_visual())
+        print(engine_wrapper.boardstate)
+        print(engine_wrapper.engine.get_evaluation())
