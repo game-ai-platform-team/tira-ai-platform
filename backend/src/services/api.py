cdb3fcb1997a563b02918cc560a965417dfd6e94
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-17 11:24:24 +0200

    refactor(17): Add initial Api class

---
+++ backend/src/services/api.py
@@ -0,0 +1,16 @@
+class Api:
+    def start(self, file: str) -> str:
+        """
+        Starts new chess game with the input AI.
+
+        Args:
+            file (str): Code of the AI.
+
+        Returns:
+            str: All moves of the game in JSON.
+        """
+
+        return ""
+
+
+api = Api()
