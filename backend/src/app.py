e3e3a80a032441a67bde8b2ada7dbb3d415ad9da
MiikaMatias <MiikaMatias@users.noreply.github.com>
2024-01-17 22:08:58 +0000

    Format code

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,20 +1,22 @@
 from flask import Flask, make_response, request, send_from_directory
 from flask_cors import CORS
-from services.api import Api
 
-from services.api import api
+from services.api import Api, api
 
 app = Flask("game-ai-testing-platform")
 CORS(app)
+
 
 @app.route("/api/chess/submit", methods=["POST"])
 def api_submit():
     json_object = api.start(request.data.decode("utf-8"))
     return json_object, 200
 
+
 @app.route("/<path:path>")
 def default(path):
     return send_from_directory("./../frontend/dist", path)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
