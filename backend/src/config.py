f7ea61a91d0997bea584e27532419818668d9efe
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-17 19:22:16 +0200

    refactor: Rename rootdir to ROOTDIR

--- backend/src/config.py
+++ backend/src/config.py
@@ -2,13 +2,13 @@ from pathlib import Path
 from os import getenv
 from dotenv import load_dotenv
 
-rootdir = Path(__file__).parent.parent
+ROOTDIR = Path(__file__).parent.parent
 
 try:
-    load_dotenv(dotenv_path=rootdir / ".env")
+    load_dotenv(dotenv_path=ROOTDIR / ".env")
 except FileNotFoundError:
     pass
 
 TEMP_DIRNAME = getenv("TEMP_DIRNAME") or "temp"
 
-TEMP_DIR = rootdir / TEMP_DIRNAME
+TEMP_DIR = ROOTDIR / TEMP_DIRNAME
