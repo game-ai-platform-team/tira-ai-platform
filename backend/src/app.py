8bfec2ba4b1b8f0f76515d0d05096faa0b6f9032
EljasV <EljasV@users.noreply.github.com>
2024-01-17 14:14:16 +0000

    Format code

--- backend/src/app.py
+++ backend/src/app.py
@@ -7,7 +7,7 @@ app = Flask("game-ai-testing-platform")
 CORS(app)
 
 
-@app.route("/api/submit", methods = ["POST"])
+@app.route("/api/submit", methods=["POST"])
 def api_submit():
     if not request.json:
         return "", 400
@@ -29,4 +29,4 @@ def default(path):
 
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 5000)
+    app.run(host="0.0.0.0", port=5000)
