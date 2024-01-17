63d4a44dd9a7b7b89af7dd2ec2326258558ceaf9
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-17 12:55:38 +0200

    refactor(17): Initialize Player class
    Co-authored-by: mleikas <101827229+mleikas@users.noreply.github.com>

---
+++ backend/src/entities/player.py
@@ -0,0 +1,13 @@
+class Player:
+    def play(self, move: str) -> str:
+        """
+        Plays a turn.
+
+        Args:
+            move (str): Move of the other player.
+
+        Returns:
+            str: Move of player.
+        """
+
+        return move
