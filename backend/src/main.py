022c357bb368245eac0178cc27214074532fb01b
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-17 12:52:48 +0200

    refactor(17): Move __main__ from chess.py to main.py
    Co-authored-by: mleikas <101827229+mleikas@users.noreply.github.com>

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,18 +7,26 @@ import time
 
 from stockfish_path import stockfish_path
 from utils.engine_wrapper import EngineWrapper
+from services.games.chess import Chess
 
 if __name__ == "__main__":
-    print(os.getcwd())
-    print("Hello world!! Let's play chess :DDDDDDDDD")
+    if True:
+        sf_path = stockfish_path()
+        ew = EngineWrapper([], 5, sf_path)
+        c = Chess("test_ai.py", "test_ai_random.py", ew)
+        c.play(1000, 0.1)
 
-    path = stockfish_path()
-    engine_wrapper = EngineWrapper([], 5, path)
+    if False:
+        print(os.getcwd())
+        print("Hello world!! Let's play chess :DDDDDDDDD")
 
-    for _ in range(999):
-        best_move = engine_wrapper.calculate_move()
-        engine_wrapper.add_moves([best_move])
-        time.sleep(0.1)
-        print(engine_wrapper.engine.get_board_visual())
-        print(engine_wrapper.boardstate)
-        print(engine_wrapper.engine.get_evaluation())
+        path = stockfish_path()
+        engine_wrapper = EngineWrapper([], 5, path)
+
+        for _ in range(999):
+            best_move = engine_wrapper.calculate_move()
+            engine_wrapper.add_moves([best_move])
+            time.sleep(0.1)
+            print(engine_wrapper.engine.get_board_visual())
+            print(engine_wrapper.boardstate)
+            print(engine_wrapper.engine.get_evaluation())
