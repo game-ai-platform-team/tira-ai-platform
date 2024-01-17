ec2990595e3548a791b9c323707e4229b65e26fe
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-17 19:55:12 +0200

    refactor(#17): Remove unused methods

--- backend/src/services/games/chess.py
+++ backend/src/services/games/chess.py
@@ -42,44 +42,6 @@ class Chess:
 
         self.print_board()
 
-    def validate(self, move: str) -> bool:
-        """
-        Validates the move.
-
-        Args:
-            move (str): Move of a player.
-
-        Returns:
-            bool: True if game continues, False if player of the move lost.
-        """
-
-        try:
-            self.engine_wrapper.engine.make_moves_from_current_position([move])
-        except ValueError:
-            return False
-
-        return True
-
-    def add_move(self, move: str) -> None:
-        """
-        Adds move to the board.
-
-        Args:
-            move (str): Move to add.
-        """
-
-        self.engine_wrapper.boardstate.append(move)
-
-    def get_board(self) -> list[str]:
-        """
-        Return current board.
-
-        Returns:
-            list[str]: Board as list of moves.
-        """
-
-        return self.engine_wrapper.boardstate
-
     def print_board(self):
         print(self.judger.get_visual_board())
         print(self.judger.get_board())
