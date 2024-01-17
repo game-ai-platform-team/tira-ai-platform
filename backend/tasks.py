9bc0729c25db3b650cad5597b13af427880a3bfa
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-17 11:52:08 +0200

    ci: Add test and lint invoke task

--- backend/tasks.py
+++ backend/tasks.py
@@ -5,3 +5,11 @@ from invoke.tasks import task
 def format(ctx):
     ctx.run("poetry run black .")
     ctx.run("poetry run isort .")
+
+@task
+def test(ctx):
+    ctx.run("poetry run coverage run --branch -m pytest src/tests")
+
+@task
+def lint(ctx):
+    ctx.run("poetry run pylint src --fail-under=8")
