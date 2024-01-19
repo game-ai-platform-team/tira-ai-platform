c46f32615d140bb9994d763747edd2dfc5bb8736
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-19 17:23:25 +0200

    refactor(#17): Move playing one turn to own method

--- backend/src/services/games/chess.py
+++ backend/src/services/games/chess.py
@@ -26,28 +26,13 @@ class Chess:
         winner = None
 
         for _ in range(turns):
-            white_move = self.player1.play(self.judger.get_board())
-
-            if not self.judger.validate(white_move):
-                winner = "player2"
-                print("White lost")
-
-                break
-
-            self.judger.add_move(white_move)
-
-            black_move = self.player2.play(self.judger.get_board())
-
-            if not self.judger.validate(black_move):
-                winner = "player1"
-                print("Black lost")
-
-                break
-
-            self.judger.add_move(black_move)
+            winner = self.__play_one_turn()
 
             self.print_board()
             time.sleep(delay)
+
+            if winner:
+                break
 
         result = {
             "winner": winner,
@@ -55,6 +40,30 @@ class Chess:
         }
 
         return result
+
+    def __play_one_turn(self) -> str:
+        winner = ""
+        white_move = self.player1.play(self.judger.get_board())
+
+        if not self.judger.validate(white_move):
+            winner = "player2"
+            print("White lost")
+
+            return winner
+
+        self.judger.add_move(white_move)
+
+        black_move = self.player2.play(self.judger.get_board())
+
+        if not self.judger.validate(black_move):
+            winner = "player1"
+            print("Black lost")
+
+            return winner
+
+        self.judger.add_move(black_move)
+
+        return winner
 
     def print_board(self):
         print(self.judger.get_visual_board())
