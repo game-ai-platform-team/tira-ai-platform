ebdd200917ea107186fee489d5cdbb09b9e1f52a
EljasV <EljasV@users.noreply.github.com>
2024-01-19 17:45:47 +0000

    style: Format code

--- frontend/src/components/CodeView.tsx
+++ frontend/src/components/CodeView.tsx
@@ -1,23 +1,26 @@
 import SubmitForm from "./SubmitForm.tsx";
-import {useState} from "react";
+import { useState } from "react";
 
 interface CodeViewProps {
-    testResult?: { moves: string[]; winner: string; };
+    testResult?: { moves: string[]; winner: string };
 }
 
 function CodeView(props: CodeViewProps) {
-    const [result, setResult] = useState(props?.testResult)
+    const [result, setResult] = useState(props?.testResult);
 
-    const moves = result?.moves ? result.moves : []
-    const winnerMessage = result?.winner ? <p>winner: {result.winner}</p> : undefined
-
+    const moves = result?.moves ? result.moves : [];
+    const winnerMessage = result?.winner ? (
+        <p>winner: {result.winner}</p>
+    ) : undefined;
 
     return (
         <>
             <SubmitForm setResult={setResult}></SubmitForm>
             {winnerMessage}
             <ol id="move-list">
-                {moves.map((value, index) => <li key={index}>{value}</li>)}
+                {moves.map((value, index) => (
+                    <li key={index}>{value}</li>
+                ))}
             </ol>
         </>
     );
