b5b930de250b0b7640d838323492c35ca4c3e061
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-01-19 18:20:56 +0200

    feat(17): Add frontend for chess results

--- frontend/src/components/SubmitForm.tsx
+++ frontend/src/components/SubmitForm.tsx
@@ -1,12 +1,17 @@
-import React, { ChangeEvent, useState, JSX } from "react";
+import React, {ChangeEvent, useState, JSX} from "react";
 import axios from "axios";
+
+
+interface SubmitFormProps {
+    setResult: (result: { moves: string[], winner: string }) => void;
+}
 
 /**
  *  Component for submitting code files to server.
  *
  * @returns {JSX.Element}
  */
-function SubmitForm(): JSX.Element {
+function SubmitForm(props: SubmitFormProps): JSX.Element {
     const [file, setFile] = useState<File>();
 
     const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
@@ -15,28 +20,23 @@ function SubmitForm(): JSX.Element {
         }
     };
 
-    if (file) {
-        const p = async () => {
-            const f = await file.text();
-            console.log(f);
-        };
-        p();
-    }
 
     const onSubmit = async (e: React.SyntheticEvent) => {
         e.preventDefault();
         const baseURL = "http://localhost:5000";
         if (file) {
-            await axios.post(`${baseURL}/api/chess/submit`, {
+            const result = await axios.post(`${baseURL}/api/chess/submit`, {
                 content: await file.text(),
             });
+            console.log(result.data)
+            props.setResult(result.data as unknown as { moves: string[], winner: string });
         }
     };
 
     return (
         <>
             <form onSubmit={onSubmit}>
-                <input id="file-input" type="file" onChange={handleFile} />
+                <input id="file-input" type="file" onChange={handleFile}/>
                 <button id="submit-button" type="submit">
                     Submit
                 </button>
