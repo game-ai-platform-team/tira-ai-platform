df35a12dd5201282f68a43dbaec590a39cd43eb7
EljasV <EljasV@users.noreply.github.com>
2024-01-19 16:21:26 +0000

    style: Format code

--- frontend/src/tests/components/SubmitForm.test.tsx
+++ frontend/src/tests/components/SubmitForm.test.tsx
@@ -3,14 +3,14 @@ import { render } from "@testing-library/react";
 import SubmitForm from "../../components/SubmitForm";
 
 test("file path field exists", () => {
-    const component = render(<SubmitForm  setResult={() => {}}/>);
+    const component = render(<SubmitForm setResult={() => {}} />);
     const fileInput = component.container.querySelector("#file-input");
 
     expect(fileInput).not.toBe(null);
 });
 
 test("submit button exists", () => {
-    const component = render(<SubmitForm setResult={() => {}}/>);
+    const component = render(<SubmitForm setResult={() => {}} />);
     const submitButton = component.container.querySelector("#submit-button");
 
     expect(submitButton).not.toBe(null);
