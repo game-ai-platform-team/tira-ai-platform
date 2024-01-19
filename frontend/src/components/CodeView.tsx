34dff7c4608e24336451c126f4bc194d51a7b2ec
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-01-19 19:42:04 +0200

    test(17): Add test into frontend

--- frontend/src/components/CodeView.tsx
+++ frontend/src/components/CodeView.tsx
@@ -1,22 +1,23 @@
 import SubmitForm from "./SubmitForm.tsx";
-import { useState } from "react";
+import {useState} from "react";
 
-function CodeView() {
-    const [result, setResult] = useState<{ moves: string[]; winner: string }>();
+interface CodeViewProps {
+    testResult?: { moves: string[]; winner: string; };
+}
 
-    const moves = result?.moves ? result.moves : [];
-    const winnerMessage = result?.winner ? (
-        <p>winner: {result.winner}</p>
-    ) : undefined;
+function CodeView(props: CodeViewProps) {
+    const [result, setResult] = useState(props?.testResult)
+
+    const moves = result?.moves ? result.moves : []
+    const winnerMessage = result?.winner ? <p>winner: {result.winner}</p> : undefined
+
 
     return (
         <>
             <SubmitForm setResult={setResult}></SubmitForm>
             {winnerMessage}
-            <ol>
-                {moves.map((value, index) => (
-                    <li key={index}>{value}</li>
-                ))}
+            <ol id="move-list">
+                {moves.map((value, index) => <li key={index}>{value}</li>)}
             </ol>
         </>
     );
