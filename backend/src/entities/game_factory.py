dadda66c7d0619808ea1ec623eab741c404654a1
MiikaMatias <miikapiiparinen24@gmail.com>
2024-01-22 18:56:15 +0200

    feat(#32) game factory class polish

---
+++ backend/src/entities/game_factory.py
@@ -0,0 +1,18 @@
+
+class GameFactory:
+    @staticmethod
+    def get_game(game_type: str):
+        """
+        Get a game from this method by inserting a string
+
+        Args:
+            game_type (string): F.e. "chess" or "othello". Can be checked from GAMEDICT in config.
+
+        Returns:
+            game instance of specified class
+        """
+
+        game_class = {"chess": Chess, "othello": None}
+
+
+        return game_class()
