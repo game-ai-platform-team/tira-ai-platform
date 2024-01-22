6824bf563c5a2869e4aefefa33b1081bd5f14c0c
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-01-22 16:55:01 +0200

    feat(27): Refactor

--- frontend/src/components/BrowsableChessboard.tsx
+++ frontend/src/components/BrowsableChessboard.tsx
@@ -1,47 +1,51 @@
-import { Position } from "kokopu";
-import { Chessboard } from "kokopu-react";
-import { useState } from "react";
+import {Position} from "kokopu";
+import {Chessboard} from "kokopu-react";
+import {useEffect, useState} from "react";
 
 interface BrowsableChessboardProps {
     moves?: string[];
 }
 
+const emptyList: string[] = []
+
 export function BrowsableChessboard(props: BrowsableChessboardProps) {
-    const position = new Position();
-    const moves = props.moves ? props.moves : [];
-    const [moveNumber, setMoveNumber] = useState<number>(0);
+    new Position();
+    const moves = props.moves ? props.moves : emptyList
+    const [moveNumber, setMoveNumber] = useState<number>(0)
+    const [positions, setPositions] = useState<Position[]>([])
+
+    useEffect(() => {
+        let oldPosition = new Position()
+        const set: Position[] = [oldPosition]
+        for (let i = 0; i < moves.length; i++) {
+            const value = moves[i]
+            const newPos = new Position(oldPosition)
+            newPos.play(newPos.uci(value));
+            set.push(newPos)
+            oldPosition = newPos
+        }
+        setPositions(set)
+    }, [moves])
 
     const increaseMoveNumber = () => {
-        setMoveNumber((prevState) => {
-            const next = prevState + 1;
+        setMoveNumber(prevState => {
+            const next = prevState + 1
             return next <= moves.length ? next : prevState;
-        });
-    };
+        })
+    }
     const decreaseMoveNumber = () => {
-        setMoveNumber((prevState) => {
-            const next = prevState - 1;
+        setMoveNumber(prevState => {
+            const next = prevState - 1
             return next >= 0 ? next : prevState;
-        });
-    };
-
-    for (let i = 0; i < moves.length && i < moveNumber; i++) {
-        const value = moves[i];
-        position.play(position.uci(value));
+        })
     }
 
-    return (
-        <div style={{ width: "min-content" }}>
-            <Chessboard position={position}></Chessboard>
-            <div
-                style={{
-                    justifyContent: "space-between",
-                    flexDirection: "row",
-                    display: "flex",
-                }}
-            >
-                <button onClick={decreaseMoveNumber}>{"<"}</button>
-                <button onClick={increaseMoveNumber}>{">"}</button>
-            </div>
+
+    return <div style={{width: "min-content"}}>
+        <Chessboard position={positions[moveNumber]}></Chessboard>
+        <div style={{justifyContent: "space-between", flexDirection: "row", display: "flex"}}>
+            <button onClick={decreaseMoveNumber}>{"<"}</button>
+            <button onClick={increaseMoveNumber}>{">"}</button>
         </div>
-    );
+    </div>;
 }
