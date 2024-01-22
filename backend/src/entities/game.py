294257834600fe6fae737bfc8e126167273ad654
MiikaMatias <miikapiiparinen24@gmail.com>
2024-01-22 18:25:10 +0200

    fix: dumb 2 electric boogaloo

--- backend/src/entities/game.py
+++ backend/src/entities/game.py
@@ -3,7 +3,7 @@ from services.games.chess import Chess
 
 class GameFactory:
     @staticmethod
-    def get_animal(game_type: str):
+    def get_game(game_type: str):
         """Static method to get a specific game object"""
 
         games = {"chess": Chess, "othello": None}
