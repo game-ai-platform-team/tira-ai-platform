2fefe4aefffa695dfd988a357c7f5fc27eb41e08
EljasV <EljasV@users.noreply.github.com>
2024-01-22 14:25:55 +0000

    style: Format code

--- frontend/src/components/CodeView.tsx
+++ frontend/src/components/CodeView.tsx
@@ -1,7 +1,7 @@
 import SubmitForm from "./SubmitForm.tsx";
-import {useState} from "react";
-import {ChessGameResult} from "../types.ts";
-import {BrowsableChessboard} from "./BrowsableChessboard.tsx";
+import { useState } from "react";
+import { ChessGameResult } from "../types.ts";
+import { BrowsableChessboard } from "./BrowsableChessboard.tsx";
 
 interface CodeViewProps {
     testResult?: ChessGameResult;
@@ -24,7 +24,7 @@ function CodeView(props: CodeViewProps) {
                     <li key={index}>{value}</li>
                 ))}
             </ol>
-            <BrowsableChessboard moves={moves}/>
+            <BrowsableChessboard moves={moves} />
         </>
     );
 }
