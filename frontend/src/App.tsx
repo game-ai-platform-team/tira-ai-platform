7631a01de692445357eb2db61c272e7585bc0b76
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-01-22 16:58:19 +0200

    feat(#26): Add NavigationBar
    Co-authored-by: Rasmus <rasmus.marttila@hotmail.com>
    Co-authored-by: TheJiahao <TheJiahao@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import CodeView from "./components/CodeView.tsx";
+import NavigationBar from "./components/NavigationBar.tsx";
 
 function App() {
     return (
-        <>
-            <CodeView></CodeView>
-        </>
+        <div>
+            <NavigationBar />
+            <>
+                <CodeView></CodeView>
+            </>
+        </div>
     );
 }
 
