ab0ca4323a3b608426da143f00a43710151e382c
MiikaMatias <miikapiiparinen24@gmail.com>
2024-01-23 11:50:38 +0200

    feat(#32) add factory to main

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,11 +5,13 @@ Wraps Stockfish
 from pathlib import Path
 
 from config import ROOTDIR
-from services.games.chess import Chess
+from entities.game_factory import GameFactory
 from stockfish_engine import get_stockfish_engine
 
 if __name__ == "__main__":
     engine = get_stockfish_engine()
+
+    Chess = GameFactory.get_game("chess")
     c = Chess(
         Path(ROOTDIR / "src" / "ai.py"),
         Path(ROOTDIR / "src" / "ai.py"),
