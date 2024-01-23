e1365a3f84fd3408af0c80b49cf6ec2904a9606e
MiikaMatias <miikapiiparinen24@gmail.com>
2024-01-23 10:41:57 +0200

    refactor(#32) unify stockfish path into stockfish engine

--- backend/src/stockfish_path.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def stockfish_path():
-    path = os.path.join(
-        os.path.dirname(__file__),
-        "../engines/stockfish_ubuntu/stockfish-ubuntu-x86-64-avx2",
-    )
-    return path
