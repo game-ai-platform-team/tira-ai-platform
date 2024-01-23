40fa2f8cb7c0fb011d736f022de2c69be8d2d2ef
MiikaMatias <MiikaMatias@users.noreply.github.com>
2024-01-23 16:18:51 +0000

    style: Format code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
             <>
                 <CodeView></CodeView>
             </>
-            <JustInTimeMoveList/>
+            <JustInTimeMoveList />
         </div>
     );
 }
