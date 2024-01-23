d7207740043afa71c7f2f404ee744523cda61ad5
MiikaMatias <miikapiiparinen24@gmail.com>
2024-01-23 17:57:49 +0200

    feat(#40): modify existing frontend to accomodate POCJIT

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+$(document).ready(function(){
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+    socket.on('my response', function(msg) {
+        $('#log').append('<p>Received: ' + msg.data + '</p>');
+    });
+    $('form#emit').submit(function(event) {
+        socket.emit('my event', {data: $('#emit_data').val()});
+        return false;
+    });
+});
