062d60c1997845c240a3b5a57963399f9e302ec8
skeltal2 <skeltal2@users.noreply.github.com>
2024-01-23 13:53:28 +0000

    Format code

--- backend/src/entities/chess_judger.py
+++ backend/src/entities/chess_judger.py
@@ -17,7 +17,7 @@ class ChessJudger:
 
         if move not in legal_moves:
             return GameState.ILLEGAL
-        
+
         self.add_move(move)
 
         if self.board.is_checkmate():
@@ -34,12 +34,12 @@ class ChessJudger:
     def is_valid_uci_move(self, uci_move):
         pattern = re.compile(r"^[a-h][1-8][a-h][1-8][qrbn]?$")
         return bool(pattern.match(uci_move))
-    
+
     def add_move(self, move):
         self.board.push(chess.Move.from_uci(move))
 
     def get_board_visual(self):
         return str(self.board)
-    
+
     def get_moves_as_uci(self):
         return [move.uci() for move in self.board.move_stack]
