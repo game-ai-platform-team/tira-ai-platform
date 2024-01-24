289afd2a86cf9a0d3f45714824694736ac59c20b
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-01-24 12:13:01 +0200

    feat(#26): Add SubmitForm.css

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -6,29 +6,41 @@ const NavigationBar = () => {
 
     const handleGameChange = (game) => {
         setSelectedGame(game);
-        //Placeholder - add what happens when game is changed
+        // Placeholder - add what happens when the game is changed
     };
 
     return (
         <div id="navigation-bar">
             <div className="dropdown">
                 <button className="nav-button">
-                    <span role="img" aria-label="Submit">
-                        &#x1F4E6;
+                    <span role="img" aria-label="GameIcon">
+                        {getGameIcon(selectedGame)}
                     </span>{" "}
                     {selectedGame}
                 </button>
                 <div className="dropdown-content">
                     <button onClick={() => handleGameChange("Chess")}>
+                        <span role="img" aria-label="GameIcon">
+                            ♟️
+                        </span>{" "}
                         Chess
                     </button>
                     <button onClick={() => handleGameChange("Gomoku")}>
+                        <span role="img" aria-label="GameIcon">
+                            🌀
+                        </span>{" "}
                         Gomoku
                     </button>
                     <button onClick={() => handleGameChange("Othello")}>
+                        <span role="img" aria-label="GameIcon">
+                            ⚪
+                        </span>{" "}
                         Othello
                     </button>
                     <button onClick={() => handleGameChange("Connect4")}>
+                        <span role="img" aria-label="GameIcon">
+                            🔴
+                        </span>{" "}
                         Connect4
                     </button>
                 </div>
@@ -41,6 +53,22 @@ const NavigationBar = () => {
             </button>
         </div>
     );
+};
+
+// Function to get the appropriate icon for each game
+const getGameIcon = (game) => {
+    switch (game) {
+        case "Chess":
+            return "♟️";
+        case "Gomoku":
+            return "🌀";
+        case "Othello":
+            return "⚪";
+        case "Connect4":
+            return "🔴";
+        default:
+            return "";
+    }
 };
 
 export default NavigationBar;
