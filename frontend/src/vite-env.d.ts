4b75fdc1e5a635d2746f49707f011459f178cec2
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-01-24 17:19:30 +0200

    fix: lint

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="vite/client" />
 $(document).ready(function () {
-    var socket = io.connect(
-        "http://" + document.domain + ":" + location.port + "/test",
+    const socket = io.connect(
+        "http://" + document.domain + ":" + location.port + "/test"
     );
     socket.on("my response", function (msg) {
         $("#log").append("<p>Received: " + msg.data + "</p>");
     });
-    $("form#emit").submit(function (event) {
+    $("form#emit").submit(function () {
         socket.emit("my event", { data: $("#emit_data").val() });
         return false;
     });
