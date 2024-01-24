35d603c23dffd967d8d078457b4180b25e7a63bb
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2024-01-24 12:46:28 +0200

    feat(#46): Add Game class

---
+++ backend/src/services/games/game.py
@@ -0,0 +1,36 @@
+import time
+
+from entities.player import Player
+from game_state import GameState
+
+
+class Game:
+    def __init__(self, socketio, player1_file, player2_file):
+        self.socketio = socketio
+        self.player1 = Player(player1_file)
+        self.player2 = Player(player2_file)
+
+        self.turn_counter = 0
+        self.last_player = None
+
+    def play(self):
+        pass
+
+    def play_one_move(self, player: Player, prev_move):
+        start_time = time.perf_counter()
+        move = player.play(prev_move)
+        end_time = time.perf_counter() - start_time
+        state = self.judger.validate(move)
+        self.check_state(state, move)
+
+        return state, move, end_time
+
+    def socket(self, move):
+        self.socketio.emit("newmove", {"move": move}, namespace="/movereceiver")
+
+    def check_state(self, state, move):
+        if state == GameState.ILLEGAL:
+            move = None
+        if state == GameState.INVALID:
+            move = None
+        self.socket(move)
