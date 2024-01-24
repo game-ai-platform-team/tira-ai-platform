44681e2e34e758769a50891d1d70b2bd2fbc00cb
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-01-24 11:07:00 +0200

    feat(#26): Add dropdown menu to navigationbar

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,14 +1,38 @@
+import React, { useState } from "react";
 import "./NavigationBar.css";
 
 const NavigationBar = () => {
+    const [selectedGame, setSelectedGame] = useState("Chess");
+
+    const handleGameChange = (game) => {
+        setSelectedGame(game);
+        //Placeholder - add what happens when game is changed
+    };
+
     return (
         <div id="navigation-bar">
-            <button className="nav-button">
-                <span role="img" aria-label="Submit">
-                    &#x1F4E6;
-                </span>{" "}
-                Submit
-            </button>
+            <div className="dropdown">
+                <button className="nav-button">
+                    <span role="img" aria-label="Submit">
+                        &#x1F4E6;
+                    </span>{" "}
+                    {selectedGame}
+                </button>
+                <div className="dropdown-content">
+                    <button onClick={() => handleGameChange("Chess")}>
+                        Chess
+                    </button>
+                    <button onClick={() => handleGameChange("Gomoku")}>
+                        Gomoku
+                    </button>
+                    <button onClick={() => handleGameChange("Othello")}>
+                        Othello
+                    </button>
+                    <button onClick={() => handleGameChange("Connect4")}>
+                        Connect4
+                    </button>
+                </div>
+            </div>
             <button className="nav-button">
                 <span role="img" aria-label="Feedback">
                     &#x1F4AC;
