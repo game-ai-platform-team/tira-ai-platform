e18b301859e5e4d4e3d4f538ede3071cb1e974c2
EljasV <EljasV@users.noreply.github.com>
2024-01-24 15:20:19 +0000

    style: Format code

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 $(document).ready(function () {
     const socket = io.connect(
-        "http://" + document.domain + ":" + location.port + "/test"
+        "http://" + document.domain + ":" + location.port + "/test",
     );
     socket.on("my response", function (msg) {
         $("#log").append("<p>Received: " + msg.data + "</p>");
