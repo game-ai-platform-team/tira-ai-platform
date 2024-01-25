9c420e658a351363662bf70ca6a3bf84d108ce85
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-25 16:27:57 +0200

    refactor(#48): Move game to services

--- backend/src/services/game_factory.py
+++ backend/src/services/game_factory.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from config import DEFAULT_CHESS_AI_PATH
 from entities.player import Player
 from services.games.chess_game import ChessGame
-from services.games.game import Game
+from services.game import Game
 from services.socket_io_service import SocketIOService
 from entities.chess_judge import ChessJudge
 
