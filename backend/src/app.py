6de2d5903771134986b0a226f8b4645d250b27d5
TheJiahao <TheJiahao@users.noreply.github.com>
2024-01-25 15:07:34 +0000

    Format code

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 from flask_socketio import SocketIO
+
 from services.api import api
 
 app = Flask("game-ai-testing-platform")
