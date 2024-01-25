3708e7d7468685677d565674a93ac525c7e8b0b8
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-25 15:24:39 +0200

    feat(#48): Add get_chess_game

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,6 +3,7 @@ Wraps Stockfish
 """
 
 from pathlib import Path
+from unittest.mock import Mock
 
 from config import ROOTDIR
 from entities.game_factory import GameFactory
@@ -11,9 +12,10 @@ from stockfish_engine import get_stockfish_engine
 if __name__ == "__main__":
     engine = get_stockfish_engine()
 
-    Chess = GameFactory.get_game("chess")
-    c = Chess(
+    chess_game = GameFactory.get_chess_game(
         Path(ROOTDIR / "src" / "ai.py"),
         Path(ROOTDIR / "src" / "ai.py"),
+        Mock(),
     )
-    c.play(1000, 0.1, debug=True)
+
+    chess_game.play(1000, 0.1, debug=True)
