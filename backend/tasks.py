672a5c4b92157ead0e6394f1410bc8e77f7b076b
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-01-26 12:46:40 +0200

    chore(#49): Add tasks/scripts for generating coverage report

--- backend/tasks.py
+++ backend/tasks.py
@@ -13,5 +13,15 @@ def test(ctx):
 
 
 @task
+def coverage_html(ctx):
+    ctx.run("coverage run --branch -m pytest src && coverage html", pty=True)
+
+
+@task
+def coverage_xml(ctx):
+    ctx.run("coverage run --branch -m pytest src && coverage xml", pty=True)
+
+
+@task
 def lint(ctx):
     ctx.run("poetry run pylint src --fail-under=8")
