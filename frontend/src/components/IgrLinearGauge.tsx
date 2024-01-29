f8b267249f69844aaea894c4bfc4cba587ebd9e3
MiikaMatias <miikapiiparinen24@gmail.com>
2024-01-29 14:33:26 +0200

    fix(#36): add keys to range components

--- frontend/src/components/IgrLinearGauge.tsx
+++ frontend/src/components/IgrLinearGauge.tsx
@@ -58,6 +58,7 @@ function LinearGauge() {
                 rangeOutlines="#C62828, #F96232, #FF9800"
             >
                 <IgrLinearGraphRange
+<<<<<<< HEAD
                     startValue={0}
                     endValue={50}
                     innerStartExtent={0.075}
@@ -73,6 +74,17 @@ function LinearGauge() {
                     outerStartExtent={0.4}
                     outerEndExtent={0.55}
                 />
+=======
+                    key="range1" // Unique key for the first range
+                    startValue={0} endValue={50}
+                    innerStartExtent={0.075} innerEndExtent={0.075}
+                    outerStartExtent={0.25} outerEndExtent={0.4} />
+                <IgrLinearGraphRange
+                    key="range2" // Unique key for the second range
+                    startValue={50} endValue={100}
+                    innerStartExtent={0.075} innerEndExtent={0.075}
+                    outerStartExtent={0.4} outerEndExtent={0.55} />
+>>>>>>> fix(#36): add keys to range components
             </IgrLinearGauge>
         </div>
     );
