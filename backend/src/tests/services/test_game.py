ca4a422e6baa55dbd48518ce3b565a93d52a01ec
skeltal2 <skeltal2@users.noreply.github.com>
2024-01-31 11:22:56 +0000

    Format code

--- backend/src/tests/services/test_game.py
+++ backend/src/tests/services/test_game.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import Mock, call, ANY
+from unittest.mock import ANY, Mock, call
 
 from entities.move import Move
 from game_state import GameState
