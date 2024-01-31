6dc8b5a730b0e2761eadd5b7fc5b7f5ff1dbc414
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-01-31 16:15:29 +0200

    feat(#36) Cleaning code

--- frontend/src/components/EvaluationBar.tsx
+++ frontend/src/components/EvaluationBar.tsx
@@ -6,8 +6,6 @@ interface EvaluationBarProps {
 }
 
 const EvaluationBar: React.FC<EvaluationBarProps> = ({ linePosition = 0 }) => {
-<<<<<<< HEAD
-    // Ensure linePosition is within the range [-1, 1]
     const clampedLinePosition = Math.max(-1, Math.min(1, linePosition));
 
     return (
@@ -21,21 +19,8 @@ const EvaluationBar: React.FC<EvaluationBarProps> = ({ linePosition = 0 }) => {
                     top: `calc(${(clampedLinePosition + 1) * 50}% - 2px)`,
                 }}
             ></div>
-            {/* Add any additional content or styling if needed */}
         </div>
     );
-=======
-  const clampedLinePosition = Math.max(-1, Math.min(1, linePosition));
-
-  return (
-    <div className="evaluation-bar">
-      <div className="division-line top"></div>
-      <div className="division-line middle"></div>
-      <div className="division-line bottom"></div>
-      <div className="red-line" style={{ top: `calc(${(clampedLinePosition + 1) * 50}% - 2px)` }}></div>
-    </div>
-  );
->>>>>>> 05ef717 (feat(#36) Cleaning code)
 };
 
 export default EvaluationBar;
