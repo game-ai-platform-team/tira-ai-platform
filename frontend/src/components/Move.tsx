b2d2d2cc839c4f75d30522b86608041ec893d624
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-02-01 12:59:18 +0200

    feat(#25): Make move toggleable

--- frontend/src/components/Move.tsx
+++ frontend/src/components/Move.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
 interface MoveProps {
     move: string;
     time: number;
     state: string;
 }
 
-const Move = ({move, time, state}: MoveProps) => {
-    return <span>Move:{move}</span>;
+const Move = ({ move, time, state }: MoveProps) => {
+    const [visible, setVisible] = useState(false)
+    const showWhenVisible = { display: visible ? "" : "none"}
+        return (
+        <div className="move">
+            <a onClick={() => setVisible(!visible) }><span>Move: {move}</span></a>
+            
+            <div style={showWhenVisible}>
+                
+                <span>Time: {time}</span>
+                <span>State: {state}</span>{" "}
+            </div>
+        </div>
+    );
 };
 
-
-export default Move
-export type {MoveProps}
+export default Move;
+export type { MoveProps };
