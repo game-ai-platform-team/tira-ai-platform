c8e93723de5f8752cc0d0e84fbe9b70ce1fc894e
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-01 14:21:28 +0200

    test(#25): Add more tests for Move component
    Co-authored-by: skeltal2 <skeltal2@users.noreply.github.com>

--- frontend/src/tests/components/Move.test.tsx
+++ frontend/src/tests/components/Move.test.tsx
@@ -3,10 +3,47 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Move from "../../components/Move";
 
-describe("move component", async () => {
-    const container = render(<Move move={"e1e2"} time={100} />).container;
+describe("move component", () => {
+    let container: HTMLElement;
+
+    beforeEach(() => {
+        container = render(<Move move={"e1e2"} time={100} />).container;
+    });
 
     test("move shown", () => {
         expect(screen.queryByText("e1e2")).not.toBeNull;
+    });
+
+    test("details is hidden by default", () => {
+        const details = container.querySelector(".move-details");
+
+        expect(details).toHaveStyle("display: none");
+    });
+
+    test("details is shown after click", async () => {
+        const user = userEvent.setup();
+        const moveContent = container.querySelector(
+            ".move-content",
+        ) as HTMLElement;
+
+        await user.click(moveContent);
+
+        const details = container.querySelector(".move-details");
+
+        expect(details).not.toHaveStyle("display: none");
+    });
+
+    test("details can be hidden after click", async () => {
+        const user = userEvent.setup();
+        const moveContent = container.querySelector(
+            ".move-content",
+        ) as HTMLElement;
+
+        await user.click(moveContent);
+        await user.click(moveContent);
+
+        const details = container.querySelector(".move-details");
+
+        expect(details).toHaveStyle("display: none");
     });
 });
