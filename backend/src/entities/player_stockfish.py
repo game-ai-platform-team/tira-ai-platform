e37eb2a1dc524e652b8edbaa5c7bb2a4e718f33a
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-02-05 18:36:12 +0200

    feat(#68): Add stockfish player
    Co-authored-by: Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
    Co-authored-by: Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>

---
+++ backend/src/entities/player_stockfish.py
@@ -0,0 +1,17 @@
+from stockfish_engine import get_stockfish_engine
+
+class PlayerStockfish():
+    def __init__(self, level) -> None:
+        self.engine = get_stockfish_engine()
+        self.boardstate = []
+        self.engine.set_skill_level(level)
+
+    def play(self, move):
+        self.boardstate.append(move)
+        self.engine.set_position(self.boardstate)
+        new_move = self.engine.get_best_move()
+        self.boardstate.append(new_move)
+        return new_move
+    
+    def terminate_self(self):
+        pass
