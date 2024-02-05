6ffd9743855bf7a85f576c2e63f8dffdbcdc8098
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-02-05 18:36:12 +0200

    feat(#68): Add stockfish player
    Co-authored-by: Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
    Co-authored-by: Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>

--- backend/src/services/game_factory.py
+++ backend/src/services/game_factory.py
@@ -5,18 +5,31 @@ from entities.chess_judge import ChessJudge
 from entities.player import Player
 from services.game import Game
 from services.socket_io_service import SocketIOService
+from entities.player_stockfish import PlayerStockfish
 
 
 class GameFactory:
     @staticmethod
     def get_chess_game(
         socketio_service: SocketIOService,
-        player1_file: Path = DEFAULT_CHESS_AI_PATH,
-        player2_file: Path = DEFAULT_CHESS_AI_PATH,
+        player1_file = None,
+        player2_file = None,
+        level = 0,
     ) -> Game:
+        
+        if player1_file is None:
+            player1 = PlayerStockfish(level)
+        else:
+            player1 = Player(player1_file) 
+
+        if player2_file is None:
+            player2 = PlayerStockfish(level)
+        else:
+            player2 = Player(player2_file)
+
         return Game(
             socketio_service,
-            Player(player1_file),
-            Player(player2_file),
+            player1,
+            player2,
             ChessJudge(),
         )
