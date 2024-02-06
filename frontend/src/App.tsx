f7931a9a2568fbbd965fc1fe69209b80485b8548
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-06 16:23:56 +0200

    refactor(#73): Make ChessGameView void tag without empty parent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,7 @@ function App() {
     return (
         <div>
             <NavigationBar />
-            <>
-                <ChessGameView></ChessGameView>
-            </>
+            <ChessGameView />
         </div>
     );
 }
