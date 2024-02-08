15c73cc89258befb2814fb2719383ab5d7e51438
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-08 12:56:10 +0200

    refactor(#73): Draw AdvantageBar with SVG

--- frontend/src/components/AdvantageBar.tsx
+++ frontend/src/components/AdvantageBar.tsx
@@ -1,24 +1,78 @@
-import React from "react";
 import "./AdvantageBar.css";
 
 interface AdvantageBarProps {
     linePosition?: number;
+    width?: number;
+    height?: number;
+    topColor?: string;
+    botColor?: string;
 }
 
-const AdvantageBar: React.FC<AdvantageBarProps> = ({ linePosition = 0 }) => {
-    const clampedLinePosition = Math.max(-1, Math.min(1, linePosition));
+const AdvantageBar = ({
+    linePosition = 0,
+    width = 50,
+    height = 500,
+    topColor = "#b5876b",
+    botColor = "#f0dec7",
+}: AdvantageBarProps) => {
+    const indicatorPosition = ((linePosition + 1) / 2) * height;
 
     return (
         <div className="advantage-bar">
-            <div className="division-line top"></div>
-            <div className="division-line middle"></div>
-            <div className="division-line bottom"></div>
-            <div
-                className="red-line"
-                style={{
-                    top: `calc(${(clampedLinePosition + 1) * 50}% - 2px)`,
-                }}
-            ></div>
+            <svg
+                width={width}
+                height={height}
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <defs>
+                    <linearGradient
+                        id="background"
+                        x1="0%"
+                        x2="0%"
+                        y1="0%"
+                        y2="100%"
+                    >
+                        <stop offset="0%" stopColor={topColor} />
+                        <stop offset="100%" stopColor={botColor} />
+                    </linearGradient>
+                </defs>
+                <rect
+                    width="100%"
+                    height="100%"
+                    rx="10px"
+                    ry="10px"
+                    fill="url(#background)"
+                    style={{ stroke: "black", strokeWidth: "2px" }}
+                />
+                <line
+                    x1="0%"
+                    x2="100%"
+                    y1="25%"
+                    y2="25%"
+                    style={{ stroke: "black", width: "2px" }}
+                />
+                <line
+                    x1="0%"
+                    x2="100%"
+                    y1="50%"
+                    y2="50%"
+                    style={{ stroke: "black", width: "2px" }}
+                />
+                <line
+                    x1="0%"
+                    x2="100%"
+                    y1="75%"
+                    y2="75%"
+                    style={{ stroke: "black", width: "2px" }}
+                />
+                <line
+                    x1="0%"
+                    x2="100%"
+                    y1={indicatorPosition}
+                    y2={indicatorPosition}
+                    style={{ stroke: "red", strokeWidth: "5px" }}
+                />
+            </svg>
         </div>
     );
 };
