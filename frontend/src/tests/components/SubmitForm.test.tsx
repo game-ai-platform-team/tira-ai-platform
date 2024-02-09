bcaf68f625e1e8908018e064ec1adad84bcd6901
EljasV <EljasV@users.noreply.github.com>
2024-02-09 12:01:42 +0000

    style: Format code

--- frontend/src/tests/components/SubmitForm.test.tsx
+++ frontend/src/tests/components/SubmitForm.test.tsx
@@ -2,27 +2,21 @@ import { render } from "@testing-library/react";
 import SubmitForm from "../../components/SubmitForm";
 
 test("file path field exists", () => {
-    const component = render(
-        <SubmitForm />
-    );
+    const component = render(<SubmitForm />);
     const fileInput = component.container.querySelector("#file-input");
 
     expect(fileInput).not.toBe(null);
 });
 
 test("submit button exists", () => {
-    const component = render(
-        <SubmitForm />
-    );
+    const component = render(<SubmitForm />);
     const submitButton = component.container.querySelector("#submit-button");
 
     expect(submitButton).not.toBe(null);
 });
 
 test("elo slider exists", () => {
-    const component = render(
-        <SubmitForm />
-    );
+    const component = render(<SubmitForm />);
     const fileInput = component.container.querySelector("#elo-config");
 
     expect(fileInput).not.toBe(null);
