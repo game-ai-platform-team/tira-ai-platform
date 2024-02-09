b9ed83f6a1324d1c1d2d156e79b85ea98af060f7
EljasV <EljasV@users.noreply.github.com>
2024-02-09 14:34:38 +0000

    style: Format code

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import GameReducer from "./reducers/GameReducer";
 import moveReducer from "./reducers/moveReducer";
 
-
 const store = configureStore({
     reducer: { moves: moveReducer, game: GameReducer },
 });
