09773a8d0a3f0a8c574a604639acb2dec73dbf97
EljasV <EljasV@users.noreply.github.com>
2024-02-09 12:01:42 +0000

    style: Format code

--- frontend/src/tests/components/GameView.test.tsx
+++ frontend/src/tests/components/GameView.test.tsx
@@ -7,11 +7,12 @@ import store from "../../store.ts";
 import { createMove } from "../../reducers/moveReducer.ts";
 
 test("move list gets rendered", async () => {
-    store.dispatch(createMove({ move: "c2c3", logs: "", time: 100, advantage: 0 }));
+    store.dispatch(
+        createMove({ move: "c2c3", logs: "", time: 100, advantage: 0 }),
+    );
     const ui = (
         <Provider store={store}>
-            <GameView
-            />
+            <GameView />
         </Provider>
     );
     const component = render(ui);
