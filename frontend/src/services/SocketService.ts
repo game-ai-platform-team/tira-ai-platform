c77342bb286563d7033b7436c0295ffe5673a4e5
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-02-09 14:05:52 +0200

    refactor(#73): remove GameConnection
    Co-authored-by: mleikas <mleikas@users.noreply.github.com>

--- frontend/src/services/SocketService.ts
+++ frontend/src/services/SocketService.ts
@@ -1,35 +1,8 @@
-import { io, Socket } from "socket.io-client";
+import { io } from "socket.io-client";
 import store from "../store";
 import { createMove } from "../reducers/moveReducer";
 import { GameConfig } from "../types.ts";
 import MoveStatistics from "../interfaces/MoveStatistics.ts";
-
-console.log("socket");
-
-export class GameConnection {
-    private socket: Socket;
-
-    constructor() {
-        this.socket = io("/gameconnection");
-    }
-
-    isConnected(): boolean {
-        return this.socket.connected;
-    }
-
-    postcode(file: string, elo: number) {
-        this.socket.emit("postcode", { content: file, elo });
-    }
-
-    connect() {
-        this.socket.connect();
-    }
-
-    disconnect() {
-        console.log("disconnected");
-        this.socket.disconnect();
-    }
-}
 
 export function startGame(config: GameConfig) {
     const socket = io("/gameconnection");
