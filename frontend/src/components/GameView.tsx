3e4d887db17a14c3fc646b1ee217dae5149dbfab
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-02-12 15:42:39 +0200

    feat(#59): Create PGN string from game

--- frontend/src/components/GameView.tsx
+++ frontend/src/components/GameView.tsx
@@ -11,13 +11,26 @@ import AdvantageBar from "./AdvantageBar.tsx";
 import store from "../store.ts";
 import { ReactNode } from "react";
 import { useAppSelector } from "../hook.ts";
+import { Game, Position, pgnWrite } from "kokopu";
 
 function GameView({ children }: { children: ReactNode }) {
     const winnerMessage = <p>winner: {"testwinner"}</p>;
     const moveIndex = useAppSelector((state) => state.boardIndex) - 1;
+    const moveStats = useAppSelector((state) => state.moves)
+    const boards = useAppSelector((state) => state.boards)
 
     const stats = getStatistics(store.getState().moves);
     const evals = getEvaluations(store.getState().moves, true);
+    
+    const game = new Game();
+    let current = game.mainVariation();
+    for (let i = 0; i < moveStats.length; i++) {
+        const position = new Position(boards[i]);
+        const move = moveStats[i].move;
+        const san = position.notation(position.uci(move));
+        current = current.play(san);
+    }
+    const pgnString = pgnWrite(game);
 
     return (
         <div id="game-view">
