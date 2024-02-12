25a170a14ee36d7b8639adbafdbc297404d2195b
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-02-12 15:41:47 +0200

    feature(#78): implement expand/hide mark
    Co-authored-by: emlyy <emlyy@users.noreply.github.com>
    Co-authored-by: mleikas@helsinki.fi <max.o.leikas@helsinki.fi>

--- frontend/src/components/Move.tsx
+++ frontend/src/components/Move.tsx
@@ -4,9 +4,12 @@ import MoveStatistics from "../interfaces/MoveStatistics.ts";
 const Move = ({ move, logs, time }: MoveStatistics) => {
     const [visible, setVisible] = useState(false);
     const showWhenVisible = { display: visible ? "" : "none" };
+    const dropdownImage = visible ? "dropdown_mark_open.svg" : "dropdown_mark.svg";
+
     return (
         <div className="move">
             <a className="move-content" onClick={() => setVisible(!visible)}>
+                <img src={dropdownImage} />
                 <span>Move: {move}</span>
             </a>
 
