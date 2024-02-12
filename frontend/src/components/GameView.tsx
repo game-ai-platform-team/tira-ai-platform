d6010ae97903c65ab28755acbbc46afbb57f93eb
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-12 13:22:10 +0200

    fix: AdvantageBar not updating
    Co-authored-by: mleikas <max.o.leikas@helsinki.fi>

--- frontend/src/components/GameView.tsx
+++ frontend/src/components/GameView.tsx
@@ -10,9 +10,11 @@ import TimeChart from "./TimeChart.tsx";
 import AdvantageBar from "./AdvantageBar.tsx";
 import store from "../store.ts";
 import { ReactNode } from "react";
+import { useAppSelector } from "../hook.ts";
 
 function GameView({ children }: { children: ReactNode }) {
     const winnerMessage = <p>winner: {"testwinner"}</p>;
+    const moveIndex = useAppSelector((state) => state.boardIndex) - 1;
 
     const stats = getStatistics(store.getState().moves);
     const evals = getEvaluations(store.getState().moves, true);
@@ -22,7 +24,7 @@ function GameView({ children }: { children: ReactNode }) {
             <SubmitForm />
             {children}
             <div id="winner-message">{winnerMessage}</div>
-            <AdvantageBar linePosition={evals.advantages.at(-1)} />
+            <AdvantageBar linePosition={evals.advantages.at(moveIndex)} />
 
             <MoveList />
 
