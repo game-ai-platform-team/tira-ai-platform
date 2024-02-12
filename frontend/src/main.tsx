a8cec32be09b95e6e33fe6814d62f8b259ebab71
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-12 10:52:48 +0200

    style: Format code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,17 +4,16 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./store.ts";
 
-
-const root = ReactDOM.createRoot(document.getElementById("root")!)
-const renderApp = () =>{
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+const renderApp = () => {
     root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
-);
-}
+        <React.StrictMode>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </React.StrictMode>,
+    );
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
--- frontend/src/reducers/boardReducer.ts
+++ frontend/src/reducers/boardReducer.ts
@@ -6,28 +6,36 @@ console.log("game");
 
 const boardSlice = createSlice({
     name: "board",
-    initialState: { position: [new Position()], boardIndex: 0},
+    initialState: { position: [new Position()], boardIndex: 0 },
     reducers: {
         newBoard(state, action: PayloadAction<MoveStatistics>) {
-            const oldPosition: Position = state.position[state.position.length-1] as Position
-            const newPos = new Position (oldPosition)
+            const oldPosition: Position = state.position[
+                state.position.length - 1
+            ] as Position;
+            const newPos = new Position(oldPosition);
             newPos.play(newPos.uci(action.payload.move));
             const newArray = [...state.position, newPos];
-            return { ...state, position: newArray, moveNumber: newArray.length-1};
+            return {
+                ...state,
+                position: newArray,
+                moveNumber: newArray.length - 1,
+            };
         },
         increaseMoveNumber(state) {
             const next = state.boardIndex + 1;
-            const clamped = next < state.position.length ? next : state.boardIndex;
-            return { ...state, moveNumber: clamped}
+            const clamped =
+                next < state.position.length ? next : state.boardIndex;
+            return { ...state, moveNumber: clamped };
         },
         decreaseMoveNumber(state) {
             const next = state.boardIndex - 1;
             const clamped = next >= 0 ? next : state.boardIndex;
-            return { ...state, moveNumber: clamped}
-        }
+            return { ...state, moveNumber: clamped };
+        },
     },
 });
 
 export default boardSlice.reducer;
 
-export const { newBoard, increaseMoveNumber, decreaseMoveNumber} = boardSlice.actions;
+export const { newBoard, increaseMoveNumber, decreaseMoveNumber } =
+    boardSlice.actions;
