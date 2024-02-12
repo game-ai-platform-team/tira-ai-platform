72d084fd97cff128567b5d7d3935ece7f82cd9a6
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-12 10:33:06 +0200

    feat: Add pretyped dispatch and selector
    Co-authored-by: mleikas <max.o.leikas@helsinki.fi>

---
+++ frontend/src/hook.ts
@@ -0,0 +1,6 @@
+import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "./store";
+
+type DispatchFunc = () => AppDispatch;
+export const useAppDispatch: DispatchFunc = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
