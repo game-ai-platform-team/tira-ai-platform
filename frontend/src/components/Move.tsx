ef0e2b7a3d5f056e9dee8b24f394e045eef7dae0
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-02-12 12:58:09 +0200

    refactor: remove move props

--- frontend/src/components/Move.tsx
+++ frontend/src/components/Move.tsx
@@ -1,24 +1,21 @@
 import { useState } from "react";
 import MoveStatistics from "../interfaces/MoveStatistics.ts";
 
-interface MoveProps {
-    statistics: MoveStatistics;
-}
+const Move = ({ move, logs, time }: MoveStatistics) => {
 
-const Move = ({ statistics }: MoveProps) => {
     const [visible, setVisible] = useState(false);
     const showWhenVisible = { display: visible ? "" : "none" };
     return (
         <div className="move">
             <a className="move-content" onClick={() => setVisible(!visible)}>
-                <span>Move: {statistics.move}</span>
+                <span>Move: {move}</span>
             </a>
 
             <div className="move-details" style={showWhenVisible}>
                 <p />
-                <span>Time: {statistics.time}</span>
+                <span>Time: {time}</span>
                 <p />
-                <span>Logging: {statistics.logs} </span>
+                <span>Logging: {logs} </span>
                 <p />
             </div>
         </div>
@@ -26,4 +23,3 @@ const Move = ({ statistics }: MoveProps) => {
 };
 
 export default Move;
-export type { MoveProps };
