011dd8a63a3d519f9e9ff03d62673dc7c68cf7be
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-12 14:00:19 +0200

    test(#73): Add partial tests for ChessBoard

--- frontend/src/tests/components/Chessboard.test.tsx
+++ frontend/src/tests/components/Chessboard.test.tsx
@@ -4,33 +4,60 @@ import userEvent from "@testing-library/user-event";
 import store from "../../store.ts";
 import { createMove } from "../../reducers/moveReducer.ts";
 import { newBoard } from "../../reducers/boardReducer.ts";
+import { Provider } from "react-redux";
 
-test("Chess board changes when button clicked", async () => {
-    store.dispatch(
-        createMove({ move: "c2c3", logs: "", time: 100, evaluation: 0 }),
+describe("Chessboard", () => {
+    beforeEach(() => {
+        store.dispatch(
+            createMove({
+                move: "c2c3",
+                logs: "",
+                time: 100,
+                evaluation: 0,
+            }),
+        );
+        store.dispatch(
+            newBoard({ move: "c2c3", logs: "", time: 100, evaluation: 0 }),
+        );
+    });
+
+    const ui = (
+        <Provider store={store}>
+            <Chessboard />
+        </Provider>
     );
-    store.dispatch(
-        newBoard({ move: "c2c3", logs: "", time: 100, evaluation: 0 }),
-    );
-    const ui = <Chessboard />;
-    const component = render(ui);
-    const chessboard = component.container.querySelector(".kokopu-chessboard");
-    const boardState1 = chessboard?.outerHTML;
-    expect(boardState1).not.toBeUndefined();
 
-    const user = userEvent.setup();
+    test("is rendered", () => {
+        
+    })
 
-    const previousButton = component.container.querySelector(
-        "#previousChessboardButton",
-    );
+    test("Chess board changes when button clicked", async () => {
 
-    expect(previousButton).not.toBeNull();
+        const component = render(ui);
+        const chessboard =
+            component.container.querySelector(".kokopu-chessboard");
+        const boardState1 = chessboard?.outerHTML;
+        expect(boardState1).not.toBeUndefined();
 
-    await user.click(previousButton as Element);
+        const user = userEvent.setup();
 
-    const chessboard2 = component.container.querySelector(".kokopu-chessboard");
-    const boardState2 = chessboard2?.outerHTML;
-    expect(boardState2).not.toBeUndefined();
+        const previousButton = component.container.querySelector(
+            "#previousChessboardButton",
+        );
 
-    expect(boardState1).not.toEqual(boardState2);
+        expect(previousButton).not.toBeNull();
+
+        await user.click(previousButton as Element);
+
+        const chessboard2 = component.container.querySelector(
+            ".kokopu-chessboard",
+        ) as Element;
+        const boardState2 = chessboard2?.outerHTML;
+
+        console.log(boardState1);
+        console.log(boardState2);
+        expect(chessboard2).not.toBeUndefined();
+
+        expect(boardState1).not.toEqual(boardState2);
+    });
 });
