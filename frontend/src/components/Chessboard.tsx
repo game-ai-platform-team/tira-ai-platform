459b756c41677cad1ad746e7073ad72a5b74293e
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-02-12 10:04:51 +0200

    fix(#59): Fix arrows being one turn late

--- frontend/src/components/Chessboard.tsx
+++ frontend/src/components/Chessboard.tsx
@@ -1,34 +1,52 @@
 import { Chessboard as KokopuChessboard } from "kokopu-react";
 import "./Chessboard.css";
 import store from "../store";
-import { increaseMoveNumber, decreaseMoveNumber} from "../reducers/BoardReducer"
-
+import {
+    increaseMoveNumber,
+    decreaseMoveNumber,
+} from "../reducers/BoardReducer";
 
 const Chessboard = () => {
-
     let arrow: string = "G";
     if (store.getState().chessboard.moveNumber > 1) {
-        console.log(store.getState().moves)
-        console.log(store.getState().chessboard.moveNumber)
-        arrow = arrow + store.getState().moves[store.getState().chessboard.moveNumber - 2].move.slice(0, 4);
+        console.log(store.getState().moves);
+        console.log(store.getState().chessboard.moveNumber);
+        arrow =
+            arrow +
+            store
+                .getState()
+                .moves[
+                    store.getState().chessboard.moveNumber - 1
+                ].move.slice(0, 4);
     }
 
     return (
         <div id="chessboard">
             <h2 id="chessboard-header">Player1 vs Player2</h2>
             <KokopuChessboard
-                position={store.getState().chessboard.position[store.getState().chessboard.moveNumber]}
+                position={
+                    store.getState().chessboard.position[
+                        store.getState().chessboard.moveNumber
+                    ]
+                }
                 squareSize={60}
                 arrowMarkers={arrow}
             />
             <div>
                 <button
-                    onClick={() => {store.dispatch(decreaseMoveNumber())}}
+                    onClick={() => {
+                        store.dispatch(decreaseMoveNumber());
+                    }}
                     id="previousChessboardButton"
                 >
                     {"<"}
                 </button>
-                <button onClick={() => {store.dispatch(increaseMoveNumber())}} id="nextChessboardButton">
+                <button
+                    onClick={() => {
+                        store.dispatch(increaseMoveNumber());
+                    }}
+                    id="nextChessboardButton"
+                >
                     {">"}
                 </button>
             </div>
