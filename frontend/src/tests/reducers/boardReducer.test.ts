e0ed7b99dd518eb33c493c81fb41567598c477fb
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2024-02-12 17:47:24 +0200

    tests(#74): add boardReducer test
    Co-authored-by: EljasV <EljasV@users.noreply.github.com>

---
+++ frontend/src/tests/reducers/boardReducer.test.ts
@@ -0,0 +1,12 @@
+import store from "../../store.ts";
+import { newBoard } from "../../reducers/boardReducer.ts";
+
+describe("Board redux", () => {
+    test("changes board", () => {
+        store.dispatch(
+            newBoard({ move: "c2c3", logs: "", time: 100, evaluation: 0 }),
+        );
+        expect(store.getState().boards).toHaveLength(2);
+        expect(store.getState().boards.at(1)).toEqual("rnbqkbnr/pppppppp/8/8/8/2P5/PP1PPPPP/RNBQKBNR b KQkq - 0 1");
+    });
+});
