e4c959091aca7e01e83f156ff791c69f1287c199
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-13 14:29:48 +0200

    test(#74): Add tests for boardReducer

--- frontend/src/tests/reducers/boardReducer.test.ts
+++ frontend/src/tests/reducers/boardReducer.test.ts
@@ -1,7 +1,12 @@
 import store from "../../store.ts";
 import { newBoard } from "../../reducers/boardReducer.ts";
+import resetStateReducer from "../../reducers/resetReducer.ts";
 
-describe("Board redux", () => {
+describe("BoardReducer", () => {
+    beforeEach(() => {
+        store.dispatch(resetStateReducer());
+    });
+
     test("changes board", () => {
         store.dispatch(
             newBoard({ move: "c2c3", logs: "", time: 100, evaluation: 0 }),
@@ -9,6 +14,30 @@ describe("Board redux", () => {
         expect(store.getState().boards).toHaveLength(2);
         expect(store.getState().boards.at(1)).toEqual(
             "rnbqkbnr/pppppppp/8/8/8/2P5/PP1PPPPP/RNBQKBNR b KQkq - 0 1",
+        );
+    });
+
+    test("changes board multiple times", () => {
+        store.dispatch(
+            newBoard({
+                move: "d2d4",
+                logs: "something",
+                time: 123,
+                evaluation: -1,
+            }),
+        );
+        store.dispatch(
+            newBoard({
+                move: "b8c6",
+                logs: "something",
+                time: 123,
+                evaluation: -1,
+            }),
+        );
+
+        expect(store.getState().boards).toHaveLength(3);
+        expect(store.getState().boards[2]).toEqual(
+            "r1bqkbnr/pppppppp/2n5/8/3P4/8/PPP1PPPP/RNBQKBNR w KQkq - 0 1",
         );
     });
 });
