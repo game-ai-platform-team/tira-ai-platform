b8d6b58d1677b166fe17e42f6830476b59c9cf42
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-14 10:57:11 +0200

    test(#59): Add more test for getStatistics
    Co-authored-by: mleikas@helsinki.fi <max.o.leikas@helsinki.fi>

--- frontend/src/tests/services/StatisticsService.test.ts
+++ frontend/src/tests/services/StatisticsService.test.ts
@@ -8,12 +8,50 @@ describe("StatisticsService", () => {
             expect(statistics).toBeNull();
         });
 
-        test("returns null if only color 1 is null", () => {
+        test("returns null if only moves of color 1 is null", () => {
             const statistics = statisticsService.getStatistics(
                 [{ move: "aaa", time: 100, logs: "", evaluation: 0 }],
                 1,
             );
             expect(statistics).toBeNull();
+        });
+
+        test("does not return null if moves of color 0 is not empty", () => {
+            const statistics = statisticsService.getStatistics(
+                [
+                    {
+                        move: "aaa",
+                        time: 100,
+                        logs: "",
+                        evaluation: 0,
+                    },
+                ],
+                0,
+            );
+
+            expect(statistics).not.toBeNull();
+        });
+
+        test("does not return null if moves of color 1 is not empty", () => {
+            const statistics = statisticsService.getStatistics(
+                [
+                    {
+                        move: "aaa",
+                        time: 100,
+                        logs: "",
+                        evaluation: 0,
+                    },
+                    {
+                        move: "bbb",
+                        time: 10,
+                        logs: "logs of something",
+                        evaluation: 1,
+                    },
+                ],
+                1,
+            );
+
+            expect(statistics).not.toBeNull();
         });
     });
 });
