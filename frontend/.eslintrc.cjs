ffd0c100491e30628824d62dc6646fea1bc7d1a8
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-14 17:34:57 +0200

    ci: Add eslint-plugin-react

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -7,15 +7,22 @@ module.exports = {
         "plugin:react-hooks/recommended",
         "plugin:import/recommended",
         "plugin:import/typescript",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
     ],
     ignorePatterns: ["dist", ".eslintrc.cjs", "cypress.config.ts"],
     parser: "@typescript-eslint/parser",
-    plugins: ["react-refresh"],
+    plugins: ["react-refresh", "react"],
     rules: {
         "react-refresh/only-export-components": [
             "warn",
             { allowConstantExport: true },
         ],
         "import/no-named-as-default": ["off"],
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
     },
 };
