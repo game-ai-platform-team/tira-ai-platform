5f98444b99edf03044b06c7d824dab455072bdba
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-14 09:32:39 +0200

    refactor: Remove unnecessary store.subscribe

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,15 +5,11 @@ import { Provider } from "react-redux";
 import store from "./store.ts";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-const renderApp = () => {
-    root.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </React.StrictMode>,
-    );
-};
 
-renderApp();
-store.subscribe(renderApp);
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+);
