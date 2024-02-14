f933cc9498ed00b5e0a828f5065dd30c8756399f
TheJiahao <TheJiahao@users.noreply.github.com>
2024-02-14 16:05:27 +0000

    style: Format code

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 $(document).ready(function () {
     const socket = io.connect(
-        `http://${  document.domain  }:${  location.port  }/test`,
+        `http://${document.domain}:${location.port}/test`,
     );
     socket.on("my response", function (msg) {
-        $("#log").append(`<p>Received: ${  msg.data  }</p>`);
+        $("#log").append(`<p>Received: ${msg.data}</p>`);
     });
     $("form#emit").submit(function () {
         socket.emit("my event", { data: $("#emit_data").val() });
