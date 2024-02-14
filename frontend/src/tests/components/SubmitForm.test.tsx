a11558a80a9b9dbf180132b48915e265a928a845
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-14 09:23:18 +0200

    chore: Remove unused import

--- frontend/src/tests/components/SubmitForm.test.tsx
+++ frontend/src/tests/components/SubmitForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import SubmitForm from "../../components/SubmitForm";
 import { test, expect } from "vitest";
 
