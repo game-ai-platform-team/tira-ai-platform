df9613e1833f345833bf0d4d2c772b76fea1f864
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-02-16 18:57:21 +0200

    feat(#59) Added the functionality to download  CSV of the game
    Co-authored-by: skeltal2 <skeltal2@users.noreply.github.com>
    Co-authored-by: emlyy <emlyy@users.noreply.github.com>

---
+++ frontend/src/components/CSVCreater.tsx
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import MoveStatistics from "../interfaces/MoveStatistics.ts";
+
+interface CSVCreaterProps {
+    moves: MoveStatistics[] | null;
+}
+
+const CSVCreater: React.FC<CSVCreaterProps> = ({
+    moves
+}) => {
+    const [includeAdvantages, setIncludeAdvantages] = useState(true);
+    const [includeTimes, setIncludeTimes] = useState(true);
+    const [includeWhite, setIncludeWhite] = useState(true);
+    const [includeBlack, setIncludeBlack] = useState(true);
+
+    if (!moves) {
+        return null;
+    }
+
+    const convertToCSV = () => {
+        let csv = "Move,";
+        if (includeAdvantages) {
+            csv += "Advantage,";
+        }
+        if (includeTimes) {
+            csv += "Time,";
+        }
+        csv += "\n";
+        let index = 0;
+
+        moves.forEach(() => {
+            if (index % 2 == 1 && includeWhite) {
+                csv += ConvertTurnToCSV((index));
+            }
+            if (index % 2 == 0 && includeBlack) {
+                csv += ConvertTurnToCSV((index));
+            }
+            index++;
+        });
+
+        return csv;
+    };
+
+    const ConvertTurnToCSV = (i: number) => {
+        let moveAsText = "";
+
+        moveAsText+=`${moves[i].move },`;
+        
+        if (includeAdvantages) {
+            moveAsText += `${moves[i].evaluation || "0"},`;
+        }
+        if (includeTimes) {
+            moveAsText += `${moves[i].time || "0"},`;
+        }
+
+        moveAsText += "\n";
+
+        return moveAsText;
+    };
+
+    const downloadCSV = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        const csvContent = convertToCSV();
+        const blob = new Blob([csvContent], {
+            type: "text/csv;charset=utf-8;",
+        });
+        const link = document.createElement("a");
+        const url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute("download", "statistics.csv");
+        link.style.visibility = "hidden";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+    return (
+        <div className="CSVCreater-overlay">
+            <div className="CSVCreater">
+                <div className="CSVCreater-header">
+                    <form onSubmit={downloadCSV}>
+                        Elements in CSV
+                        <br />
+                        <input
+                            type="checkbox"
+                            checked={includeAdvantages}
+                            onChange={() =>
+                                setIncludeAdvantages(!includeAdvantages)
+                            }
+                        />
+                        Advantages
+                        <br />
+                        <input
+                            type="checkbox"
+                            checked={includeTimes}
+                            onChange={() => setIncludeTimes(!includeTimes)}
+                        />
+                        Times
+                        <br />
+                        <input
+                            type="checkbox"
+                            checked={includeWhite}
+                            onChange={() => setIncludeWhite(!includeWhite)}
+                        />
+                        White moves
+                        <br />
+                        <input
+                            type="checkbox"
+                            checked={includeBlack}
+                            onChange={() => setIncludeBlack(!includeBlack)}
+                        />
+                        Black moves
+                        <br />
+                        <button type={"submit"}>Download CSV</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CSVCreater;
--- frontend/src/components/GameView.tsx
+++ frontend/src/components/GameView.tsx
@@ -7,6 +7,7 @@ import { ReactNode } from "react";
 import { useAppSelector } from "../hook";
 import AdvantageChart from "./AdvantageChart";
 import TimeChart from "./TimeChart";
+import CSVCreater from "./CSVCreater.tsx"
 
 function GameView({ children }: { children: ReactNode }) {
     const moveIndex = useAppSelector((state) => state.boardIndex) - 1;
@@ -42,6 +43,7 @@ function GameView({ children }: { children: ReactNode }) {
 
             <div className="card">
                 <MoveList handleCopyPGN={handleCopyPGN} />
+                <CSVCreater moves={moves}/>
             </div>
 
             <div id="statistics" className="card">
