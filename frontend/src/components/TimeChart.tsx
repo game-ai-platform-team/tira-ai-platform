d084a55814b7997103d63399abba99603987112a
skeltal2 <skeltal2@users.noreply.github.com>
2024-02-16 12:41:35 +0000

    style: Format code

--- frontend/src/components/TimeChart.tsx
+++ frontend/src/components/TimeChart.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+} from "recharts";
 
 interface LineChartProps {
     data: number[];
@@ -8,19 +15,36 @@ interface LineChartProps {
 const TimeChart: React.FC<LineChartProps> = ({ data }) => {
     const chartData = data.map((value, index) => ({
         turn: index,
-        [index % 2 === 0 ? 'w_time' : 'b_time']: value
+        [index % 2 === 0 ? "w_time" : "b_time"]: value,
     }));
 
     return (
-        <LineChart width={800} height={400} data={chartData} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
-            <XAxis dataKey="turn"/>
+        <LineChart
+            width={800}
+            height={400}
+            data={chartData}
+            margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+        >
+            <XAxis dataKey="turn" />
             <YAxis />
             <Tooltip />
             <CartesianGrid stroke="#f5f5f5" />
-            <Line connectNulls type="monotone" dataKey="w_time" stroke="#d6baa9" fill="white" />
-            <Line connectNulls type="monotone" dataKey="b_time" stroke="#706056" fill="black" />
+            <Line
+                connectNulls
+                type="monotone"
+                dataKey="w_time"
+                stroke="#d6baa9"
+                fill="white"
+            />
+            <Line
+                connectNulls
+                type="monotone"
+                dataKey="b_time"
+                stroke="#706056"
+                fill="black"
+            />
         </LineChart>
-    )
+    );
 };
 
 export default TimeChart;
