4ab2e93c1c6189dde93d454c620996654f554816
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-16 13:03:44 +0200

    ci: Configure BASE_URL for staging
    Co-authored-by: EljasV <EljasV@users.noreply.github.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,15 @@
 /// <reference types="vitest" />
 
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+
+const mode = process.env.MODE;
+const env = loadEnv(mode, process.cwd());
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    mode,
+    base: env.VITE_BASE_URL,
     plugins: [react()],
     test: {
         environment: "jsdom",
