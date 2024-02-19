6af44d5decbb437df85c27f428c00fafb1f3036e
EljasV <EljasV@users.noreply.github.com>
2024-02-19 14:18:49 +0000

    Format code

--- backend/src/services/game_factory.py
+++ backend/src/services/game_factory.py
@@ -2,20 +2,20 @@ from pathlib import Path
 
 from config import DEFAULT_CHESS_AI_PATH
 from entities.chess_judge import ChessJudge
+from entities.cloned_repository import ClonedRepository
 from entities.player import Player
 from entities.player_stockfish import PlayerStockfish
 from services.game import Game
 from services.socket_io_service import SocketIOService
-from entities.cloned_repository import ClonedRepository
 
 
 class GameFactory:
     @staticmethod
     def get_chess_game(
-            socketio_service: SocketIOService,
-            player1_file = None,
-            player2_file = None,
-            elo = 1350,
+        socketio_service: SocketIOService,
+        player1_file=None,
+        player2_file=None,
+        elo=1350,
     ) -> Game:
         if player1_file is None:
             player1 = PlayerStockfish(elo)
@@ -35,5 +35,7 @@ class GameFactory:
         )
 
     @staticmethod
-    def get_github_chess_game(socketio_service: SocketIOService, repo: ClonedRepository, elo = 1350):
+    def get_github_chess_game(
+        socketio_service: SocketIOService, repo: ClonedRepository, elo=1350
+    ):
         return Game(socketio_service, Player(repo), PlayerStockfish(elo), ChessJudge())
