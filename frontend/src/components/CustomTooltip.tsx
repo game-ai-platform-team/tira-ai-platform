a84dc7497d87466be639b5cad66290b579b5b6ad
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-02-19 12:58:04 +0200

    feat(#59): Show move in tooltip

--- frontend/src/components/CustomTooltip.tsx
+++ frontend/src/components/CustomTooltip.tsx
@@ -3,17 +3,19 @@ import type {
     ValueType,
 } from "recharts/types/component/DefaultTooltipContent";
 import { TooltipProps } from "recharts";
+import { useAppSelector } from "../hook";
 
 export const CustomTooltip = ({
     active,
     payload,
     label,
 }: TooltipProps<ValueType, NameType>) => {
+    const moves = useAppSelector((state) => state.moves)
     if (active && payload && payload.length) {
         const emoji = label === 0 ? "🔴" : label % 2 === 0 ? "⚫" : "⚪";
         return (
             <div className="custom-tooltip">
-                <p className="label">{`${label} ${emoji} : ${payload[0].value}`}</p>
+                <p className="label">{`${label} : ${moves && moves[Math.max(label-1, 0)].move} ${emoji} : ${payload[0].value}`}</p>
             </div>
         );
     }
