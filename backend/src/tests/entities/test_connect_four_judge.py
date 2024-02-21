9a7a7af464d462010debf3171980ca26190cabb8
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-02-21 13:47:20 +0200

    feat(#96) CfourJudge addMove
    Co-authored-by: Emi <emlyy@users.noreply.github.com>

--- backend/src/tests/entities/test_connect_four_judge.py
+++ backend/src/tests/entities/test_connect_four_judge.py
@@ -8,10 +8,13 @@ from game_state import GameState
 class TestConnectFourJudge(unittest.TestCase):
     def setUp(self) -> None:
         self.judge = ConnectFourJudge()
+
+        self.board_empty_board = [[0] * 6 for i in range(7)]
+
         self.board_one_column_full = [[1, 2, 1, 2, 1, 2]]
         self.board_one_column_full.extend([[0] * 6 for i in range(6)])
 
-        self.board_one_column_one_move = [[0, 0, 0, 0, 0, 1]]
+        self.board_one_column_one_move = [[1, 0, 0, 0, 0, 0]]
         self.board_one_column_one_move.extend([[0] * 6 for i in range(6)])
 
     def test_move_not_convertable_to_int_is_invalid(self):
@@ -43,3 +46,11 @@ class TestConnectFourJudge(unittest.TestCase):
 
         judge = ConnectFourJudge(self.board_one_column_one_move)
         self.assertEqual(judge.validate("0"), GameState.CONTINUE)
+
+    def test_getboard_gets_board(self):
+        self.assertEqual(self.judge.get_board(), self.board_empty_board)
+
+    def test_add_move_adds_move(self):
+        self.judge.add_move(0)
+
+        self.assertEqual(self.judge.get_board()[0][0], 1)
