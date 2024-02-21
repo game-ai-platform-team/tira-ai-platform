3c04b9ad4119706a851ac3f42f6f8d2f8ae1f829
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-21 12:10:20 +0200

    feat(#96): Check invalid moves
    Co-authored-by: Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
    Co-authored-by: Rasmus <rasmus.marttila@hotmail.com>

--- backend/src/entities/judges/connect_four_judge.py
+++ backend/src/entities/judges/connect_four_judge.py
@@ -7,3 +7,23 @@ class ConnectFourJudge(Judge):
     ) -> None:
         self.__board: list[list[int]] = board or [[0] * 6 for i in range(7)]
         self.__moves: list[int] = moves or []
+
+    def validate(self, move: str) -> GameState:
+        state = GameState.CONTINUE
+
+        if not self.__check_valid_move(move):
+            return GameState.INVALID
+
+        return state
+
+    def __check_valid_move(self, move: str) -> bool:
+        move_int = -1
+        try:
+            move_int = int(move)
+        except ValueError:
+            return False
+
+        if not (0 <= move_int <= len(self.__board)):
+            return False
+
+        return True
