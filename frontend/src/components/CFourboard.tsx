32987d244ae28ed8762223c1bbb63730e4f8fd99
mleikas@helsinki.fi <max.o.leikas@helsinki.fi>
2024-02-21 14:35:36 +0200

    feat(#95): Turn slider and buttons for cfour

--- frontend/src/components/CFourboard.tsx
+++ frontend/src/components/CFourboard.tsx
@@ -1,7 +1,56 @@
+import { useAppSelector } from "../hook";
+import { useState, useEffect, useCallback } from "react";
+import store from "../store";
+import { setBoardIndex } from "../reducers/boardIndexReducer";
+
 const CFourboard = () => {
+    const moves = useAppSelector((state) => state.moves);
+    const boardIndex = useAppSelector((state) => state.boardIndex);
+    const [currentMove, setCurrentMove] = useState(0);
+    const handleMoveChange = useCallback(
+        (newIndex: number) => {
+            if (newIndex >= 0 && newIndex < moves.length + 1) {
+                store.dispatch(setBoardIndex(newIndex));
+                setCurrentMove(newIndex);
+            }
+        },
+        [moves],
+    );
+    useEffect(() => {
+        handleMoveChange(boardIndex);
+    }, [boardIndex, handleMoveChange]);
+
     return (
         <div id="cfourboard" className="card">
             <h2 className="card-header">Connect Four</h2>
+            <div style={{ textAlign: "center", marginTop: "20px" }}>
+                <span>Current Turn: {currentMove}</span>
+                <br />
+                <input
+                    type="range"
+                    min="0"
+                    max={moves.length}
+                    value={currentMove}
+                    onChange={(event) =>
+                        handleMoveChange(parseInt(event.target.value))
+                    }
+                    id="moveSlider"
+                />
+                <br />
+                <button
+                    onClick={() => handleMoveChange(currentMove - 1)}
+                    id="previousChessboardButton"
+                >
+                    {"<"}
+                </button>
+                <button
+                    onClick={() => handleMoveChange(currentMove + 1)}
+                    id="nextChessboardButton"
+                >
+                    {">"}
+                </button>
+                <br />
+            </div>
         </div>
     );
 };
