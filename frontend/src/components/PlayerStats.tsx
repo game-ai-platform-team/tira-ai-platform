f7a206e1ba1b05097d31867b0163f46dc6135db6
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-02-23 12:52:18 +0200

    refactor(#59): Made moves in stats clickable

--- frontend/src/components/PlayerStats.tsx
+++ frontend/src/components/PlayerStats.tsx
@@ -1,28 +1,53 @@
 import React from "react";
 import { Statistics, Evaluations } from "../services/StatisticsService";
+import MoveStatistics from "../interfaces/MoveStatistics";
+import { useDispatch } from "react-redux";
+import { setBoardIndex } from "../reducers/boardIndexReducer";
 
 interface MoveInfoProps {
     whiteStats: Statistics;
     blackStats: Statistics;
     evals: Evaluations;
+    moves: MoveStatistics[];
 }
 
 const PlayerStats: React.FC<MoveInfoProps> = ({
     whiteStats,
     blackStats,
     evals,
+    moves,
 }) => {
+    const dispatch = useDispatch();
+
+    const bLong = blackStats.longest.move;
+    const bShort = blackStats.shortest.move;
+    const wLong = whiteStats.longest.move;
+    const wShort = whiteStats.shortest.move;
+
+    const findMoveIndex = (move: string): number => {
+        return moves.findIndex((item) => item.move === move) + 1;
+    };
+
+    const bLongIndex = findMoveIndex(bLong);
+    const bShortIndex = findMoveIndex(bShort);
+    const wLongIndex = findMoveIndex(wLong);
+    const wShortIndex = findMoveIndex(wShort);
+
+    const handleMoveClick = (index: number) => {
+        dispatch(setBoardIndex(index));
+    };
+
     return (
         <div>
             <h2 className="card-header">Black Stats</h2>
             <div>
                 <p>Accuracy: {evals.accuracyBlack} %</p>
-                <p>
-                    Longest Move: {blackStats.longest.move} @{" "}
+                <p onClick={() => handleMoveClick(bLongIndex)}>
+                    Longest Move: {bLongIndex}. {bLong} @{" "}
                     {blackStats.longest.time} ms
                 </p>
-                <p>
-                    Shortest Move: {blackStats.shortest.move} @{" "}
+                <p onClick={() => handleMoveClick(bShortIndex)}>
+                    Shortest Move: {bShortIndex}. {bShort} @{" "}
                     {blackStats.shortest.time} ms
                 </p>
                 <p>Average: {Math.round(blackStats.average)} ms</p>
@@ -30,12 +55,12 @@ const PlayerStats: React.FC<MoveInfoProps> = ({
             <h2 className="card-header">White Stats</h2>
             <div>
                 <p>Accuracy: {evals.accuracyWhite} %</p>
-                <p>
-                    Longest Move: {whiteStats.longest.move} @{" "}
+                <p onClick={() => handleMoveClick(wLongIndex)}>
+                    Longest Move: {wLongIndex}. {wLong} @{" "}
                     {whiteStats.longest.time} ms
                 </p>
-                <p>
-                    Shortest Move: {whiteStats.shortest.move} @{" "}
+                <p onClick={() => handleMoveClick(wShortIndex)}>
+                    Shortest Move: {wShortIndex}. {wShort} @{" "}
                     {whiteStats.shortest.time} ms
                 </p>
                 <p>Average: {Math.round(whiteStats.average)} ms</p>
