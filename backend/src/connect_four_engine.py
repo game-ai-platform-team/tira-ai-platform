4556544de35417b7c32a3b4bd7d17571dc966b7b
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-02-23 15:13:27 +0200

    feat(#96) Cfour architechture implemented.

---
+++ backend/src/connect_four_engine.py
@@ -0,0 +1,34 @@
+
+import random
+from entities.move import Move
+from services.games.connect_four import ConnectFour
+
+
+
+class ConnectFourEngine:
+    def __init__(self, rows=6, columns=7):
+        self.rows = rows
+        self.columns = columns
+        self.game = ConnectFour(rows, columns)
+    
+    def make_move(self, move:str):
+        self.game.drop_piece(int(move))
+    
+    def get_best_move(self) -> str:
+        valid_moves =self.game.get_valid_locations()
+        
+        for column in valid_moves:
+            board = self.get_game_state()
+            for row in range(len(board[0])):
+                if self.board[column][row] == 0:
+                    if self.game.check_win(row, column):
+                        return column
+                    break
+                
+        return str(random.choice(valid_moves))
+    
+    def get_game_state(self) -> list[list[int]]:
+        return self.game.get_board
+    
+if __name__ == "__main__":
+    engine = ConnectFourEngine()
