f4d1e1f720fdf2c61e1750778745959b84a23652
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2024-02-23 11:40:00 +0200

    test(#94): Add backend unit test

--- backend/src/tests/services/test_api.py
+++ backend/src/tests/services/test_api.py
@@ -1,5 +1,6 @@
 import os
 from unittest import TestCase
+from unittest.mock import Mock
 
 from config import TEMP_DIR
 from services.api import Api
@@ -10,3 +11,8 @@ class TestApi(TestCase):
         if not os.path.exists(TEMP_DIR):
             os.mkdir(TEMP_DIR)
         self.api = Api()
+
+    def test_failing_git_clone(self):
+        socket_mock = Mock()
+        self.api.start(socket_mock, "https://github.com/aaaa", 1000, "chess")
+        socket_mock.send_error.assert_called_once()
