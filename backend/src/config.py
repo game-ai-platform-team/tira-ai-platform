0efdf773b52c7b08c1d7530d35149cc9a5e24d8a
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-02-28 13:03:49 +0200

    feat(#24): Load OIDC client id and secret from .env.secret

--- backend/src/config.py
+++ backend/src/config.py
@@ -8,6 +8,7 @@ ROOTDIR = Path(__file__).parent.parent
 
 try:
     load_dotenv(dotenv_path=ROOTDIR / ".env")
+    load_dotenv(dotenv_path=ROOTDIR / ".env.secret")
 except FileNotFoundError:
     pass
 
@@ -16,3 +17,6 @@ TEMP_DIR = Path(os.getenv("HOME")) / "temp"
 DEFAULT_CHESS_AI_FILENAME = getenv("DEFAULT_CHESS_AI_FILENAME") or "ai.py"
 DEFAULT_CHESS_AI_PATH = ROOTDIR / "src" / DEFAULT_CHESS_AI_FILENAME
 DEFAULT_CHESS_TIMEOUT = float(getenv("CHESS_TIMEOUT"))
+
+OIDC_CLIENT_ID = getenv("OIDC_CLIENT_ID")
+OIDC_CLIENT_SECRET = getenv("OIDC_CLIENT_SECRET")
