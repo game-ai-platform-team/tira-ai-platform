bdcb8ff34772e5364673261790358a673dcc81ef
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2024-02-29 16:47:24 +0200

    feat(#96): fix minimax

--- backend/src/connect_four_engine.py
+++ backend/src/connect_four_engine.py
@@ -20,6 +20,12 @@ class ConnectFourEngine:
         if len(self.judge.get_all_moves()) <= 2:
             return int(self.columns / 2)
 
+        best_move = self.max_value(-1000, 1000, self.depth)
+        print(best_move)
+
+        return str(best_move[0])
+
+    def vanha_get_best(self):
         value = 0
         best_value = -10000
         print(f"sorted list: {self.sorted_list}")
@@ -41,35 +47,49 @@ class ConnectFourEngine:
         num_list.sort(key=lambda x: abs(x - mid_index))
         return num_list
 
-    def max_value(self, alpha: int, beta: int, depth: int) -> str:
-        if self.pruning_judge.is_game_over() or depth == 0:
-            return self.pruning_judge.evaluate_board() * -1 * depth
-        value = -1000
+    def max_value(self, alpha: int, beta: int, depth: int) -> tuple:
+        print("we got here")
+        best_move = None
+        if self.pruning_judge.is_game_over() != GameState.CONTINUE or depth == 0:
+            print("we also got here")
+            return None, self.pruning_judge.evaluate_board() * -1 * (depth + 1)
+        best_value = -1000
         for column in self.sorted_list:
+            print("maximizing player loop")
             if self.pruning_judge.validate(str(column)) != GameState.CONTINUE:
                 continue
             self.pruning_judge.add_move(str(column))
-            value = max(value, self.min_value(alpha, beta, depth - 1) * depth)
-            alpha = max(alpha, value)
+            new_value = self.min_value(alpha, beta, depth - 1)[1]
+            if new_value > best_value:
+                best_move = column
+                best_value = new_value
+            alpha = max(alpha, new_value)
             if alpha >= beta:
+                self.pruning_judge.remove_latest()
                 break
             self.pruning_judge.remove_latest()
-        return value
+        return best_move, best_value
 
-    def min_value(self, alpha: int, beta: int, depth: int) -> str:
-        if self.pruning_judge.is_game_over() or depth == 0:
-            return self.pruning_judge.evaluate_board() * depth
-        value = 1000
+    def min_value(self, alpha: int, beta: int, depth: int) -> tuple:
+        best_move = None
+        if self.pruning_judge.is_game_over() != GameState.CONTINUE or depth == 0:
+            return None, self.pruning_judge.evaluate_board() * (depth + 1)
+        best_value = 1000
         for column in self.sorted_list:
+            print("minimizing player loop")
             if self.pruning_judge.validate(str(column)) != GameState.CONTINUE:
                 continue
             self.pruning_judge.add_move(str(column))
-            value = min(value, self.max_value(alpha, beta, depth - 1) * depth)
-            beta = min(beta, value)
+            new_value = self.max_value(alpha, beta, depth - 1)[1]
+            if new_value < best_value:
+                best_move = column
+                best_value = new_value
+            beta = min(beta, new_value)
             if alpha >= beta:
+                self.pruning_judge.remove_latest()
                 break
             self.pruning_judge.remove_latest()
-        return value
+        return best_move, best_value
 
 
 if __name__ == "__main__":
@@ -80,8 +100,12 @@ if __name__ == "__main__":
         test_move = input("move: ")
         engine1.make_move(test_move)
         print(f"board {engine1.judge.get_board()}")
-        test_move = engine1.get_best_move()
-        engine1.make_move(test_move)
+        print(f"board {engine1.pruning_judge.get_board()}")
+        move = engine1.get_best_move()
+        print(f"this move{move}")
+        if move:
+            print(f"{move}")
+            engine1.make_move(move)
         print(f"board {engine1.judge.get_board()}")
 
     ## DOESN'T COUNT VALUE CORRECTLY
