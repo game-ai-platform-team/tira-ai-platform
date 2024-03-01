5524dadaf27f108a08ae70e57886ea1b88bef633
MiikaMatias <miikapiiparinen24@gmail.com>
2024-03-01 12:42:29 +0200

    feat(#24): redirect path variable

--- backend/src/config.py
+++ backend/src/config.py
@@ -17,8 +17,9 @@ DEFAULT_CHESS_AI_FILENAME = getenv("DEFAULT_CHESS_AI_FILENAME") or "chess_ai.py"
 DEFAULT_CHESS_AI_PATH = ROOTDIR / "src" / DEFAULT_CHESS_AI_FILENAME
 DEFAULT_CHESS_TIMEOUT = float(getenv("CHESS_TIMEOUT") or 5)
 
-OIDC_CLIENT_ID = getenv("OIDC_CLIENT_ID")
-OIDC_CLIENT_SECRET = getenv("OIDC_CLIENT_SECRET")
+OIDC_CLIENT_ID = "aiuser"
+OIDC_CLIENT_SECRET = "test"
+OIDC_REDIRECT_PATH = getenv("OIDC_REDIRECT_PATH") or "https://localhost:5000"
 
 # if not (OIDC_CLIENT_ID and OIDC_CLIENT_SECRET):
 # raise KeyError("OIDC client id or secret missing")
