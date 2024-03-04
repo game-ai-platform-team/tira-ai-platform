1f43515daecd069147e70998403c7769e4a34bb3
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-04 16:16:23 +0200

    test(#96): Add tests for make_move
    Co-authored-by: Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>

--- backend/src/tests/test_connect_four_engine.py
+++ backend/src/tests/test_connect_four_engine.py
@@ -6,4 +6,18 @@ from connect_four_engine import ConnectFourEngine
 
 class TestGame(TestCase):
     def setUp(self) -> None:
-        self.engine = ConnectFourEngine()
+        self.judge_mock = Mock()
+        self.pruning_judge_mock = Mock()
+        self.engine = ConnectFourEngine(
+            judge=self.judge_mock, pruning_judge=self.pruning_judge_mock
+        )
+
+    def test_make_move_adds_move_to_judges(self):
+        self.engine.make_move("1")
+        self.engine.make_move("5")
+        self.engine.make_move("2")
+
+        self.judge_mock.add_move.assert_has_calls([call("1"), call("5"), call("2")])
+        self.pruning_judge_mock.add_move.assert_has_calls(
+            [call("1"), call("5"), call("2")]
+        )
