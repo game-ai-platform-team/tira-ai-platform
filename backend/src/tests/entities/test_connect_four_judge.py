f087a972e485d9a4137d34d98a05da757355f7a1
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-03-04 18:42:05 +0200

    feat(#96) Completed Cfour heuristic and added final tests for it. Next the numerical evaluation needs to  be optimized

--- backend/src/tests/entities/test_connect_four_judge.py
+++ backend/src/tests/entities/test_connect_four_judge.py
@@ -262,9 +262,11 @@ class TestConnectFourJudge(unittest.TestCase):
     def test_evaluate_empty_board(self):
         self.assertEqual(self.judge.evaluate_board(), 0)
 
-    def test_evaluate_board_after_one_move(self):
+    def test_evaluate_board_after_some_moves_move(self):
         self.judge.add_move(3)
-        self.assertEqual(self.judge.evaluate_board(), -12)
+        self.assertEqual(self.judge.evaluate_board(), -14)
+        self.judge.add_move(3)
+        self.assertEqual(self.judge.evaluate_board(), 6)
 
     def test_evaluate_horizontal_with_one_players_moves(self):
         for i in [0, 0, 1, 1]:
@@ -299,3 +301,38 @@ class TestConnectFourJudge(unittest.TestCase):
         self.assertEqual(self.judge.vertical_windows[0][1], 0)
         self.assertEqual(self.judge.vertical_windows[0][2], 0)
         self.assertEqual(self.judge.vertical_windows[2][0], 0)
+
+    def test_evaluate_dup_with_one_players_moves(self):
+        for i in [3, 3, 3, 2, 2, 0, 1]:
+            self.judge.add_move(i)
+        self.assertEqual(self.judge.dup_windows[0][0], 2)
+        self.assertEqual(self.judge.dup_windows[1][0], -8)
+        self.assertEqual(self.judge.dup_windows[2][0], 4)
+        self.assertEqual(self.judge.dup_windows[3][0], -2)
+        self.assertEqual(self.judge.dup_windows[3][1], 2)
+        self.assertEqual(self.judge.dup_windows[3][2], -2)
+
+    def test_evaluate_dup_with_both_players_moves(self):
+        for i in [3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6]:
+            self.judge.add_move(i)
+        self.assertEqual(self.judge.dup_windows[2][0], 0)
+        self.assertEqual(self.judge.dup_windows[1][0], -4)
+        self.assertEqual(self.judge.dup_windows[0][2], 0)
+        self.assertEqual(self.judge.dup_windows[1][2], 0)
+        self.assertEqual(self.judge.dup_windows[2][2], 0)
+        self.assertEqual(self.judge.dup_windows[0][2], 0)
+
+    def test_evaluate_ddown_with_one_players_moves(self):
+        for i in [1, 2, 1, 2, 2, 1, 0, 2]:
+            self.judge.add_move(i)
+        self.assertEqual(self.judge.dup_windows[0][0], -8)
+        self.assertEqual(self.judge.dup_windows[1][1], -4)
+        self.assertEqual(self.judge.dup_windows[0][1], 4)
+        self.assertEqual(self.judge.dup_windows[2][2], -2)
+
+    def test_evaluate_ddown_with_both_players_moves(self):
+        for i in [0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3]:
+            self.judge.add_move(i)
+        self.assertEqual(self.judge.ddown_windows[0][4], 0)
+        self.assertEqual(self.judge.ddown_windows[0][3], 0)
+        self.assertEqual(self.judge.ddown_windows[1][3], 0)
