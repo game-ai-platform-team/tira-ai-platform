202618754ef1d7130a070b2ff799c8e3901cdaa5
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-03-06 12:44:09 +0200

    fix(#95): Error when no moves

--- frontend/src/components/CFourUI.tsx
+++ frontend/src/components/CFourUI.tsx
@@ -15,17 +15,19 @@ const CFourUI = ({ row, column, moves }: CFourUIProps) => {
             board.push(new Array(row).fill(0));
         }
 
-        moves.forEach((move, index) => {
-            const moveValue = parseInt(move.move);
-            if (!isNaN(moveValue)) {
-                const valueToAdd = index % 2 === 0 ? 1 : 2;
-                const boardRow = board[moveValue];
-                const colorIndex = boardRow.lastIndexOf(0);
-                if (colorIndex !== -1) {
-                    boardRow[colorIndex] = valueToAdd;
+        if (moves) {
+            moves.forEach((move, index) => {
+                const moveValue = parseInt(move.move);
+                if (!isNaN(moveValue)) {
+                    const valueToAdd = index % 2 === 0 ? 1 : 2;
+                    const boardRow = board[moveValue];
+                    const colorIndex = boardRow.lastIndexOf(0);
+                    if (colorIndex !== -1) {
+                        boardRow[colorIndex] = valueToAdd;
+                    }
                 }
-            }
-        });
+            });
+        }
 
         return board;
     };
