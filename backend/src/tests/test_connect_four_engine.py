227442a44e7c227de1de2d6d0967e25fa9ce0aa8
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2024-03-06 11:04:05 +0200

    test(#96): test ai blocks opponent's win

--- backend/src/tests/test_connect_four_engine.py
+++ backend/src/tests/test_connect_four_engine.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from unittest.mock import Mock, call
 
 from connect_four_engine import ConnectFourEngine
+from entities.connectfour.connect_four_judge import ConnectFourJudge
 
 
 class TestGame(TestCase):
@@ -12,6 +13,7 @@ class TestGame(TestCase):
             judge=self.judge_mock,
             pruning_judge=self.pruning_judge_mock,
         )
+        self.engine_with_judge = ConnectFourEngine()
 
     def test_make_move_adds_move_to_judges(self):
         self.engine.make_move("1")
@@ -32,3 +34,23 @@ class TestGame(TestCase):
 
         self.judge_mock.get_all_moves.return_value = [2, 3]
         self.assertEqual(self.engine.get_best_move(), str(3))
+
+    def test_get_best_move_blocks_opponents_win_column(self):
+        for i in ["1", "2", "1", "2", "1"]:
+            self.engine_with_judge.make_move(i)
+        self.assertEqual(self.engine_with_judge.get_best_move(), "1")
+
+    def test_get_best_move_blocks_opponents_win_row(self):
+        for i in ["0", "3", "0", "1", "3", "2"]:
+            self.engine_with_judge.make_move(i)
+        self.assertEqual(self.engine_with_judge.get_best_move(), "4")
+
+    def test_get_best_move_blocks_opponents_win_diagonal_up(self):
+        for i in ["3", "3", "3", "4", "4", "4", "5", "5", "5", "5"]:
+            self.engine_with_judge.make_move(i)
+        self.assertEqual(self.engine_with_judge.get_best_move(), "2")
+
+    def test_get_best_move_blocks_opponents_win_diagonal_down(self):
+        for i in ["3", "3", "3", "2", "2", "2", "1", "1", "1", "1"]:
+            self.engine_with_judge.make_move(i)
+        self.assertEqual(self.engine_with_judge.get_best_move(), "4")
