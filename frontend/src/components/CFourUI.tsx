b88f52fd9567b47f9a2de8f1d7e2a8d25c888069
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-03-06 12:31:16 +0200

    feat(#95): Show game in frontend UI

--- frontend/src/components/CFourUI.tsx
+++ frontend/src/components/CFourUI.tsx
@@ -1,28 +1,64 @@
+import MoveStatistics from "../interfaces/MoveStatistics";
+
 interface CFourUIProps {
     row: number;
     column: number;
+    moves: MoveStatistics[];
 }
 
-const CFourUI = ({ row, column }: CFourUIProps) => {
+const CFourUI = ({ row, column, moves }: CFourUIProps) => {
     const style = { backgroundColor: "#a0522d" };
 
-    const getCircle = (row: number, column: number) => {
+    const createBoardFromMoves = () => {
+        const board: number[][] = [];
+        for (let i = 0; i <= column; i++) {
+            board.push(new Array(row).fill(0));
+        }
+
+        moves.forEach((move, index) => {
+            const moveValue = parseInt(move.move);
+            if (!isNaN(moveValue)) {
+                const valueToAdd = index % 2 === 0 ? 1 : 2;
+                const boardRow = board[moveValue];
+                const colorIndex = boardRow.lastIndexOf(0)
+                if (colorIndex !== -1) {
+                    boardRow[colorIndex] = valueToAdd;
+                }
+            }
+        });
+
+        return board;
+    };
+
+    const getCircle = (row: number, column: number, board: number[][]) => {
+        let fillColor = "white";
+        const colorCode = board[column][row];
+        if (colorCode === 1) {
+            fillColor = "red";
+        } else if (colorCode === 2) {
+            fillColor = "yellow";
+        }
+
         return (
             <circle
                 key={`${row}-${column}`}
                 cx={column * 80 + 40}
                 cy={row * 80 + 40}
                 r={32}
-                fill="white"
+                fill={fillColor}
             ></circle>
         );
     };
+
+    const board = createBoardFromMoves()
 
     const generateCircles = () => {
         const circles = [];
         for (let columnIndex = 0; columnIndex < column; columnIndex++) {
             for (let rowIndex = 0; rowIndex < row; rowIndex++) {
-                circles.push(getCircle(rowIndex, columnIndex));
+                circles.push(
+                    getCircle(rowIndex, columnIndex, board),
+                );
             }
         }
         return circles;
