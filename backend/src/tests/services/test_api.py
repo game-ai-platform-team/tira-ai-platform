a26cbc6376886440e54bb60d5b4bb193fbcc29fb
Joel Weinitschke <joel.weinitschke@gmail.com>
2024-03-06 17:39:29 +0200

    fix: api tests not always working

--- backend/src/tests/services/test_api.py
+++ backend/src/tests/services/test_api.py
@@ -16,37 +16,48 @@ class TestApi(TestCase):
         self.valid_repo = "https://github.com/game-ai-platform-team/stupid-chess-ai"
         self.invalid_repo = "https://github.com/invalid_repo"
 
-    def tearDown(self) -> None:
-        shutil.rmtree(TEMP_DIR, ignore_errors=True)
+    def mock_player(self, player_mock):
+        player_instance_mock = player_mock.return_value.__enter__.return_value
+        player_instance_mock.__exit__.return_value = None
 
-    @patch("services.api.random.randint", return_value=1234567)
+    @patch("services.api.random.randint", return_value=1)
+    @patch("services.api.Player")
     @patch("services.api.game_factory.get_game")
-    def test_successful_git_clone(self, get_game_mock, _randint_mock):
+    def test_successful_git_clone(self, get_game_mock, player_mock, _randint_mock):
         play_mock = Mock()
         get_game_mock.return_value.play = play_mock
+        self.mock_player(player_mock)
 
         github_url = self.valid_repo
         self.api.start(self.socket_mock, github_url, 1000, "chess")
 
         play_mock.assert_called_once()
 
-    @patch("services.api.random.randint", return_value=1234567)
+    @patch("services.api.random.randint", return_value=2)
+    @patch("services.api.Player")
     @patch("services.api.game_factory.get_game")
-    def test_failing_git_clone(self, _get_game_mock, _randint_mock):
+    def test_failing_git_clone(self, _get_game_mock, player_mock, _randint_mock):
         github_url = self.invalid_repo
         self.api.start(self.socket_mock, github_url, 1000, "chess")
+        self.mock_player(player_mock)
 
         self.socket_mock.send_error.assert_called_once()
 
-    @patch("services.api.random.randint", return_value=1234567)
+    @patch("services.api.random.randint", return_value=3)
     def test_git_clone_temp_dir_creation(self, _randint_mock):
         github_url = self.valid_repo
-        self.api.git_clone(github_url)  # Call git_clone directly
-        self.assertTrue(os.path.exists(os.path.join(TEMP_DIR, "repo1234567")))
+        self.api.git_clone(github_url)
+        self.assertTrue(os.path.exists(os.path.join(TEMP_DIR, "repo3")))
+        shutil.rmtree(os.path.join(TEMP_DIR, "repo3"))
 
-    @patch("services.api.random.randint", return_value=1234567)
+    @patch("services.api.random.randint", return_value=4)
+    @patch("services.api.Player")
     @patch("services.api.game_factory.get_game")
-    def test_git_clone_temp_dir_cleanup(self, _get_game_mock, _randint_mock):
+    def test_git_clone_temp_dir_cleanup(
+        self, _get_game_mock, player_mock, _randint_mock
+    ):
         github_url = self.valid_repo
         self.api.start(self.socket_mock, github_url, 1000, "chess")
-        self.assertFalse(os.path.exists(os.path.join(TEMP_DIR, "repo1234567")))
+        self.mock_player(player_mock)
+
+        self.assertFalse(os.path.exists(os.path.join(TEMP_DIR, "repo4")))
