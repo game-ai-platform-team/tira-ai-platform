14a1f7bfb4095126fb2df75cb8783e8292571a00
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-03-06 13:12:21 +0200

    Refactor(#108) Split heuristic and judge
    Co-authored-by: Emi <emlyy@users.noreply.github.com>

--- backend/src/entities/connectfour/connect_four_judge.py
+++ backend/src/entities/connectfour/connect_four_judge.py
@@ -1,3 +1,4 @@
+from entities.connectfour.connect_four_heuristic import ConnectFourHeuristic
 from entities.judge import Judge
 from game_state import GameState
 
@@ -7,17 +8,22 @@ class ConnectFourJudge(Judge):
         self,
         moves: list[int] | None = None,
         board: list[list[int]] | None = None,
+        heuristic: ConnectFourHeuristic| None = None
     ) -> None:
         rows = 6
         columns = 7
         self.__board: list[list[int]] = board or self.initialize_board(rows, columns)
         self.__moves: list[int] = moves or []
         self.__latest_move = self.calculate_latest_move()
+<<<<<<< Updated upstream
         ## 4 tables for storing the evaluation of 4 space windows starting from each space
         self.horizontal_windows: list[list[int]] = [([0] * rows) for i in range(4)]
         self.vertical_windows: list[list[int]] = [([0] * (3)) for i in range(7)]
         self.ddown_windows: list[list[int]] = [([0] * (6)) for i in range(4)]
         self.dup_windows: list[list[int]] = [([0] * (3)) for i in range(4)]
+=======
+        self.heuristic: ConnectFourHeuristic = heuristic or ConnectFourHeuristic()
+>>>>>>> Stashed changes
 
     def initialize_board(self, rows: int, columns: int) -> list[list[int]]:
         board = [([0] * rows) for i in range(columns)]
@@ -54,7 +60,7 @@ class ConnectFourJudge(Judge):
                 self.__board[int_move][row] = (len(self.__moves)) % 2 + 1
                 self.__moves.append(int_move)
                 self.__latest_move = (int_move, row)
-                self.evaluate_relevant_windows(int_move, row)
+                self.heuristic.evaluate_relevant_windows(int_move, row, self.__board)
                 return
 
     ##Removes a move to the judge and re-evaluates relevant windows to it
@@ -64,7 +70,7 @@ class ConnectFourJudge(Judge):
 
         if move:
             self.__board[move[0]][move[1]] = 0
-            self.evaluate_relevant_windows(move[0], move[1])
+            self.heuristic.evaluate_relevant_windows(move[0], move[1], self.__board)
 
     def is_game_over(self) -> GameState:
         if self.__is_win():
@@ -112,108 +118,11 @@ class ConnectFourJudge(Judge):
 
     ##Checks a win has been found in any window
     def __is_win(self) -> bool:
-        for i in self.horizontal_windows:
-            for j in i:
-                if j == 1000 or j == -1000:
-                    return True
-        for i in self.vertical_windows:
-            for j in i:
-                if j == 1000 or j == -1000:
-                    return True
-        for i in self.dup_windows:
-            for j in i:
-                if j == 1000 or j == -1000:
-                    return True
-        for i in self.ddown_windows:
-            for j in i:
-                if j == 1000 or j == -1000:
-                    return True
-        return False
-
-    ## Recognize 4 space windows that the given coordinates are in and re-evaluates them
-    def evaluate_relevant_windows(self, col, row):
-        self.__evaluate_horizontal(col, row)
-        self.__evaluate_vertical(col, row)
-        self.__evaluate_ddown(col, row)
-        self.__evaluate_dup(col, row)
-
-    def __evaluate_horizontal(self, col, row):
-        window = [0, 0, 0, 0]
-        low_cap = max(col - 3, 0)
-        top_cap = min(col + 1, 4)
-        for i in range(low_cap, top_cap):
-            for x in range(4):
-                window[x] = self.__board[i + x][row]
-            self.horizontal_windows[i][row] = self.evaluate_single_window(window)
-
-    def __evaluate_vertical(self, col, row):
-        window = [0, 0, 0, 0]
-        low_cap = max(0, row - 3)
-        top_cap = min(row + 1, 3)
-        for i in range(low_cap, top_cap):
-            for x in range(4):
-                window[x] = self.__board[col][i + x]
-            self.vertical_windows[col][i] = self.evaluate_single_window(window)
-
-    def __evaluate_ddown(self, col, row):
-        window = [0, 0, 0, 0]
-        low_space = min(3, col, 5 - row)
-        low_cap = -1 * low_space
-        top_space = min(3, 6 - col, row)
-        top_cap = low_cap + low_space + top_space - 2
-        for i in range(low_cap, top_cap):
-            for x in range(4):
-                window[x] = self.__board[col + i + x][row - i - x]
-            self.ddown_windows[col + i][row - i] = self.evaluate_single_window(window)
-
-    def __evaluate_dup(self, col, row):
-        window = [0, 0, 0, 0]
-        low_space = min(3, col, row)
-        low_cap = -1 * low_space
-        top_space = min(6 - col, 5 - row)
-        top_cap = low_cap + low_space + top_space - 2
-        for i in range(low_cap, top_cap):
-            for x in range(4):
-                window[x] = self.__board[col + i + x][row + i + x]
-            self.dup_windows[col + i][row + i] = self.evaluate_single_window(window)
-
-    def evaluate_single_window(self, window: list[int]) -> int:
-        my_piece = 2
-        opponent_piece = 1
-        my_pieces = window.count(my_piece)
-        opponent_pieces = window.count(opponent_piece)
-        if my_pieces == 4:
-            return 1000
-        if opponent_pieces == 4:
-            return -1000
-        if my_pieces > 0 and opponent_pieces > 0:
-            return 0
-        if my_pieces > 0:
-            return 2**my_pieces
-        if opponent_pieces > 0:
-            return -1 * 2**opponent_pieces
-        return 0
-
-    ##Calculates the sum of the evaluations of all 4-space windows on the board and returns it
-    def evaluate_entire_board(self) -> int:
-        evaluation = 0
-        for i in self.horizontal_windows:
-            for j in i:
-                evaluation += j
-        for i in self.vertical_windows:
-            for j in i:
-                evaluation += j
-        for i in self.dup_windows:
-            for j in i:
-                evaluation += j
-        for i in self.ddown_windows:
-            for j in i:
-                evaluation += j
-        return evaluation
+        return self.heuristic.is_win()
 
     def evaluate_board(self):
         if self.__is_win():
             return 100
         if self.__is_draw():
             return 0
-        return self.evaluate_entire_board()
+        return self.heuristic.evaluate_entire_board()
