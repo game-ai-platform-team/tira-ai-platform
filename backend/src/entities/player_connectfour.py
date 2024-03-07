90118362fbe6b6ba5a3e2d072b2f66f672a7bcf6
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-07 17:23:43 +0200

    refactor(#108): Rename make_move -> add_move

--- backend/src/entities/player_connectfour.py
+++ backend/src/entities/player_connectfour.py
@@ -7,12 +7,12 @@ class PlayerConnectFour:
         self.elo = elo
 
     def play(self, move):
-        self.engine.make_move(move)
+        self.engine.add_move(move)
         new_move = self.engine.get_best_move()
         if new_move is None:
             new_move = self.engine.random_valid_move()
             print("random move")
-        self.engine.make_move(new_move)
+        self.engine.add_move(new_move)
         return new_move
 
     def get_and_reset_current_logs(self):
