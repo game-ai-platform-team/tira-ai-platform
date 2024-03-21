95446cc5985635ccd4dd15365bb753fd01098240
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-21 18:41:19 +0200

    fix: Remove unused imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Chessboard from "./components/Chessboard";
 import CFourboard from "./components/CFourboard";
 import Gomokuboard from "./components/Gomokuboard";
