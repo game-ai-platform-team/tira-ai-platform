681f2a46df31ef9f5da5416e2fdafcabab8ffb65
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-21 13:20:52 +0200

    fix(#113): All tests of NavigationBar

--- frontend/src/tests/components/NavigationBar.test.tsx
+++ frontend/src/tests/components/NavigationBar.test.tsx
@@ -1,48 +1,76 @@
-import { render } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import NavigationBar from "../../components/NavigationBar";
-import { vi, test, expect } from "vitest";
 
-test("feedback is on the navigation bar", () => {
-    const { queryByText } = render(
-        <NavigationBar selectedGame="" handleGameChange={vi.fn()} />,
-    );
-    const feedbackButton = queryByText("Feedback");
+describe("Navigation bar", () => {
+    let gameSelectionButton: null | HTMLElement = null;
 
-    expect(feedbackButton).not.toBeNull();
-});
+    beforeEach(() => {
+        render(<NavigationBar selectedGame="" handleGameChange={vi.fn()} />);
+        gameSelectionButton = screen.getByLabelText("Select game");
+    });
 
-test("dropdown menu has chess", () => {
-    const { queryAllByText } = render(
-        <NavigationBar selectedGame="" handleGameChange={vi.fn()} />,
-    );
-    const chessButtons = queryAllByText("Chess");
+    test("feedback is on the navigation bar", () => {
+        const feedbackButton = screen.getByText("Feedback", { exact: false });
 
-    expect(chessButtons.length).not.toBeNull();
-});
+        expect(feedbackButton).not.toBeNull();
+    });
 
-test("dropdown menu has othello", () => {
-    const { queryByText } = render(
-        <NavigationBar selectedGame="" handleGameChange={vi.fn()} />,
-    );
-    const othelloButton = queryByText("Othello");
+    describe("game selection", () => {
+        test("button exists", () => {
+            expect(gameSelectionButton).not.toBeNull();
+        });
 
-    expect(othelloButton).not.toBeNull();
-});
+        test("dropdown menu is hidden at start", () => {
+            expect(() =>
+                screen.getByLabelText("Available games"),
+            ).toThrowError();
+        });
 
-test("dropdown menu has Connect4", () => {
-    const { queryByText } = render(
-        <NavigationBar selectedGame="" handleGameChange={vi.fn()} />,
-    );
-    const cfourButton = queryByText("Connect 4");
+        describe("dropdown menu", () => {
+            let gameDropdown: null | HTMLElement = null;
 
-    expect(cfourButton).not.toBeNull();
-});
+            beforeEach(async () => {
+                const user = userEvent.setup();
+                await user.click(gameSelectionButton as HTMLElement);
 
-test("dropdown menu has gomoku", () => {
-    const { queryByText } = render(
-        <NavigationBar selectedGame="" handleGameChange={vi.fn()} />,
-    );
-    const gomokuButton = queryByText("Gomoku");
+                gameDropdown = screen.getByLabelText(
+                    "Available games",
+                ) as HTMLElement;
+            });
 
-    expect(gomokuButton).not.toBeNull();
+            test("has chess", async () => {
+                const game = within(gameDropdown!).getByText("Chess", {
+                    exact: false,
+                });
+
+                expect(game).toBeDefined();
+            });
+
+            test("has gomoku", async () => {
+                const game = within(gameDropdown!).getByText("Gomoku", {
+                    exact: false,
+                });
+
+                expect(game).toBeDefined();
+            });
+
+            test("has othello", async () => {
+                const game = within(gameDropdown!).getByText("Othello", {
+                    exact: false,
+                });
+
+                expect(game).toBeDefined();
+            });
+
+            test("has connect 4", async () => {
+                const game = within(gameDropdown!).getByText("Connect 4", {
+                    exact: false,
+                });
+
+                expect(game).toBeDefined();
+            });
+        });
+    });
 });
