754c57984d1b87e450efbaf8ef8bcf9745eaf09b
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-26 12:45:56 +0200

    feat(#115): Add connectFourBoardReducer

--- frontend/src/reducers/boardReducer.ts
+++ frontend/src/reducers/boardReducer.ts
@@ -1,11 +1,18 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { AppThunkAction } from "../store";
 import chessboardReducer, { resetChessboards } from "./board/chessboardReducer";
+import connectFourBoardReducer, {
+    resetConnectFourBoards,
+} from "./board/connectFourBoardReducer";
 
-const boardReducer = combineReducers({ chessboards: chessboardReducer });
+const boardReducer = combineReducers({
+    chessboards: chessboardReducer,
+    connectFourBoards: connectFourBoardReducer,
+});
 
 const resetBoards = (): AppThunkAction => (dispatch) => {
     dispatch(resetChessboards());
+    dispatch(resetConnectFourBoards());
 };
 
 export default boardReducer;
