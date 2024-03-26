109947d805002bc3669ccc3ddc84ef38f575db8d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-26 12:45:56 +0200

    feat(#115): Add connectFourBoardReducer

---
+++ frontend/src/reducers/board/connectFourBoardReducer.ts
@@ -0,0 +1,24 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { toNumber } from "lodash";
+import MoveStatistics from "../../interfaces/MoveStatistics";
+
+const connectFourSlice = createSlice({
+    name: "connect",
+    initialState: new Array<Array<number>>(),
+    reducers: {
+        newConnectFourBoard(state, action: PayloadAction<MoveStatistics>) {
+            const move = toNumber(action.payload);
+            const previousBoard = state[state.length - 1];
+
+            state.push(previousBoard.concat(move));
+        },
+
+        resetConnectFourBoards() {
+            return new Array<Array<number>>();
+        },
+    },
+});
+
+export default connectFourSlice.reducer;
+export const { newConnectFourBoard, resetConnectFourBoards } =
+    connectFourSlice.actions;
