0b32674b74616cda1ef7cb361be22bc2ec0a81ae
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-26 11:49:39 +0200

    test(#113): Add e2e tests for connect 4

--- frontend/cypress/e2e/connect4.cy.ts
+++ frontend/cypress/e2e/connect4.cy.ts
@@ -1,4 +1,7 @@
 describe("Connect 4 game", function () {
+    const repositoryUrl =
+        "https://github.com/game-ai-platform-team/stupid-connect-four-ai";
+
     beforeEach(() => {
         cy.visit("/");
         cy.selectGame("Connect 4");
@@ -12,5 +15,21 @@ describe("Connect 4 game", function () {
                 expect(prev.html).to.equal(current.html);
             });
         });
+    });
+
+    it("plays a game when a repository is submitted", function () {
+        cy.submitRepository(repositoryUrl);
+
+        cy.get("#game-view", { timeout: 15000 }).should(
+            "not.contain",
+            "Current Turn: 0",
+        );
+    });
+
+    it("pressing move opens move stats", function () {
+        cy.submitRepository(repositoryUrl);
+
+        cy.get(".move", { timeout: 15000 }).first().click();
+        cy.get(".move-details").contains("Time:");
     });
 });
