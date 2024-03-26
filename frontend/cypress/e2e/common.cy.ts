3c7b1d1a8fa44abe6b8c1e52593419a6cd9220b4
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-26 13:03:28 +0200

    test(#113): Add e2e tests for selecting game from dropdown

--- frontend/cypress/e2e/common.cy.ts
+++ frontend/cypress/e2e/common.cy.ts
@@ -1,3 +1,14 @@
 it("front page can be opened", function () {
     cy.visit("/");
 });
+
+describe("Navigation bar", () => {
+    beforeEach(() => {
+        cy.visit("/");
+    });
+
+    it("games can be selected", () => {
+        cy.selectGame("Connect 4");
+        cy.selectGame("Chess");
+    });
+});
