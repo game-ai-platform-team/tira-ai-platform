fe5dfcdc5362d0d4b36396baed93c376262bc3d7
MiikaMatias <miikapiiparinen24@gmail.com>
2024-03-26 13:24:09 +0200

    feat(#111): add game image to repo

---
+++ game-image/src/entities/judge.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+from game_state import GameState
+
+
+class Judge(ABC):
+    """
+    An abstract class for validating moves and generating debug infos of a game.
+    """
+
+    @abstractmethod
+    def validate(self, move: str) -> GameState:
+        """
+        Validates a move, does not change internal states.
+
+        Args:
+            move (str): Move to be validated.
+
+        Returns:
+            GameState: An integer describing game state.
+        """
+
+    @abstractmethod
+    def add_move(self, move: str) -> None:
+        """
+        Adds a move and updates internal data structures based on game.
+
+        Args:
+            move (str): The move to be added.
+        """
+
+    @abstractmethod
+    def get_debug_info(self) -> str:
+        """
+        Returns debug info such as visual board state, player advantages, etc.
+
+        Returns:
+            str: Debug info.
+        """
+
+    @abstractmethod
+    def get_all_moves(self) -> list[str]:
+        """
+        Returns all moves as string, formatted based on game.
+
+        Returns:
+            list[str]: List of moves.
+        """
