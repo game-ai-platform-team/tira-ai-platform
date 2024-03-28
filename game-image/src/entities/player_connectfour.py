153d8145ad0ce9fe9c46b35e2a5b3285e917a72f
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-28 16:57:01 +0200

    refactor: Replace Connect 4 related with connect-four-lib

--- game-image/src/entities/player_connectfour.py
+++ game-image/src/entities/player_connectfour.py
@@ -1,4 +1,4 @@
-from connect_four_engine import ConnectFourEngine
+from connect_four_lib.connect_four_engine import ConnectFourEngine
 
 
 class PlayerConnectFour:
@@ -7,8 +7,12 @@ class PlayerConnectFour:
         self.elo = elo
 
     def play(self, move):
-        self.engine.make_move(move)
+        self.engine.add_move(move)
         new_move = self.engine.get_best_move()
+        if new_move is None:
+            new_move = self.engine.get_random_move()
+            print("random move")
+        self.engine.add_move(new_move)
         return new_move
 
     def get_and_reset_current_logs(self):
