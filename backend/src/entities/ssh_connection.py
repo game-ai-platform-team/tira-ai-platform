cc3c0400481d1b64fbafa8ea278e6259d548006a
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-02 11:36:59 +0300

    refactor(#111): Inject parameters to SSHConnection

--- backend/src/entities/ssh_connection.py
+++ backend/src/entities/ssh_connection.py
@@ -7,16 +7,23 @@ from config import HPC_LOGIN_NODE, HPC_SSH_PRIVATE_KEY_PATH, HPC_USERNAME
 
 
 class SSHConnection(AbstractContextManager):
-    def __init__(self, client: SSHClient | None = None) -> None:
-        self.__client = client or SSHClient()
+    def __init__(
+        self,
+        client: SSHClient | None = None,
+        private_key: PKey | None = None,
+        hostname: str = HPC_LOGIN_NODE,
+        username: str = HPC_USERNAME,
+    ) -> None:
+        self.__client: SSHClient = client or SSHClient()
+        self.__private_key: PKey = private_key or PKey.from_path(HPC_SSH_PRIVATE_KEY_PATH)
+        self.__hostname: str = hostname
+        self.__username: str = username
 
         self.__client.set_missing_host_key_policy(AutoAddPolicy())
 
     def __enter__(self) -> "SSHConnection":
-        private_key = PKey.from_path(HPC_SSH_PRIVATE_KEY_PATH)
-
         self.__client.connect(
-            hostname=HPC_LOGIN_NODE, pkey=private_key, username=HPC_USERNAME
+            hostname=self.__hostname, pkey=self.__private_key, username=self.__username
         )
 
         return self
