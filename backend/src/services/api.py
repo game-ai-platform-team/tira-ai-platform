7c0122379703da2b32461bfbc7dd5f90158da05a
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-04 17:45:03 +0300

    refactor(#111): Use HPCService in API

--- backend/src/services/api.py
+++ backend/src/services/api.py
@@ -1,9 +1,7 @@
-from pathlib import Path
 from time import sleep
-from uuid import uuid1
 
 from entities.ssh_connection import SSHConnection
-from services.batch_builder import BatchBuilder
+from services.hpc_service import HPCService
 from services.socket_service import SocketService
 
 
@@ -18,18 +16,14 @@ class API:
         if game not in ["chess", "connect_four"]:
             return
 
-        id = str(uuid1())
-
         with SSHConnection() as connection:
-            script = BatchBuilder.create_script(game, repository_url, difficulty, id)
+            hpc = HPCService(connection)
 
-            remote_path = connection.send_file(script)
-
-            connection.execute(f"sbatch {str(remote_path)}")
+            hpc.submit(game, repository_url, difficulty)
 
             sleep(1)
 
-            print(connection.read_file(Path(f"result-{id}.txt")))
+            print(hpc.read_output())
 
 
 api = API()
