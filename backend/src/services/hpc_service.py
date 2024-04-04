5d2418d8d2e27ebd4db6093d132eca6c1e8c47f8
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-04 17:41:55 +0300

    refactor(#111): Implement HPCService.read_output

--- backend/src/services/hpc_service.py
+++ backend/src/services/hpc_service.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from uuid import uuid1
 from entities.ssh_connection import SSHConnection
 from services.batch_builder import BatchBuilder
@@ -15,3 +16,20 @@ class HPCService:
         remote_path = self.__connection.send_file(batch)
 
         self.__connection.execute(f"sbatch {remote_path}")
+
+    def read_output(self) -> list[str]:
+        """
+        Reads new lines in output file since previous read.
+
+        Returns:
+            list[str]: New lines of output file.
+        """
+
+        output_path = Path(f"result-{self.__id}.txt")
+
+        data = self.__connection.read_file(output_path)
+        new_lines = data[self.__current_output_line :]
+
+        self.__current_output_line = len(data)
+
+        return new_lines
