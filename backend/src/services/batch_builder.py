191a3e7499512da007610cf2d97f6b616bc50700
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-04 17:20:45 +0300

    fix(#111): Parameter id conflicts with builtin

--- backend/src/services/batch_builder.py
+++ backend/src/services/batch_builder.py
@@ -14,7 +14,7 @@ BATCH_CONFIG = {
 
 class BatchBuilder:
     @staticmethod
-    def create_script(repository_url: str, game: str, id: str) -> Path:
+    def create_script(repository_url: str, game: str, id_: str) -> Path:
         script = "\n".join(
             [
                 "#!/bin/bash",
@@ -23,13 +23,13 @@ class BatchBuilder:
                 f"#SBATCH --mem {BATCH_CONFIG['memory']}",
                 f"#SBATCH -t {BATCH_CONFIG['time']}",
                 f"#SBATCH -t {BATCH_CONFIG['cpu']}",
-                f"#SBATCH -o result-{id}.txt",
+                f"#SBATCH -o result-{id_}.txt",
                 f"#SBATCH --export=REPOSITORY_URL={repository_url},GAME={game}",
                 "echo 'Hello world!'",
             ]
         )
 
-        script_path = TEMP_DIR / f"batch-{id}.sh"
+        script_path = TEMP_DIR / f"batch-{id_}.sh"
 
         with open(script_path, mode="w", encoding="utf-8") as file:
             file.write(script)
