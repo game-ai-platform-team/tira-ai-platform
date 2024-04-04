c492e72affd994bfd4238d8613a7e9cf3bb56ddc
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-04 15:25:22 +0300

    feat(#111): Add initial BatchScriptBuilder

--- backend/src/services/batch_script_builder.py
+++ backend/src/services/batch_script_builder.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from config import TEMP_DIR
+
 
 BATCH_CONFIG = {
     "cluster": "ukko",
@@ -13,10 +15,23 @@ BATCH_CONFIG = {
 class BatchScriptBuilder:
     @staticmethod
     def create_script(repository_url: str, game: str, id: str) -> Path:
-        script = [
-            "#!/bin/bash",
-            f"#SBATCH -M {BATCH_CONFIG['cluster']}",
-            f"#SBATCH -p {BATCH_CONFIG['partition']}",
-            f"#SBATCH --mem {BATCH_CONFIG['memory']}",
-            f""
-        ]
+        script = "\n".join(
+            [
+                "#!/bin/bash",
+                f"#SBATCH -M {BATCH_CONFIG['cluster']}",
+                f"#SBATCH -p {BATCH_CONFIG['partition']}",
+                f"#SBATCH --mem {BATCH_CONFIG['memory']}",
+                f"#SBATCH -t {BATCH_CONFIG['time']}",
+                f"#SBATCH -t {BATCH_CONFIG['cpu']}",
+                f"#SBATCH -o result-{id}.txt",
+                f"#SBATCH --export=REPOSITORY_URL={repository_url},GAME={game}",
+                "echo 'Hello world!'",
+            ]
+        )
+
+        script_path = TEMP_DIR / f"batch-{id}.sh"
+
+        with open(script_path, mode="w", encoding="utf-8") as file:
+            file.write(script)
+
+        return script_path
