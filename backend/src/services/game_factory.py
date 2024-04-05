ef81c0a3a602bc60c67277fd7e41c2b24ddce79f
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-05 14:29:22 +0300

    refactor(#126): Only pass players to `GameFactory`

--- backend/src/services/game_factory.py
+++ backend/src/services/game_factory.py
@@ -4,35 +4,29 @@ from connect_four_lib.connect_four_judge import ConnectFourJudge
 
 from entities.chess_judge import ChessJudge
 from entities.player import Player
-from entities.player_connectfour import PlayerConnectFour
-from entities.player_stockfish import PlayerStockfish
 from services.game import Game
-from services.socket_service import SocketService
 
 
 class GameFactory:
-    def __init__(self, games: dict[str, Callable[..., Game]] | None = None) -> None:
-        self.__games: dict[
-            str, Callable[[SocketService, int, Player], Game]
-        ] = games or {
-            "chess": GameFactory.__get_chess_game,
-            "connect_four": GameFactory.__get_connect_four_game,
-        }
-
-    def get_game(
+    def __init__(
         self,
-        socket_service: SocketService,
-        game: str,
-        player: Player,
-        elo: int = 1350,
-    ) -> Game:
+        games: dict[str, Callable[[Player, Player], Game]] | None = None,
+        logger: Callable[[str], None] | None = None,
+    ) -> None:
+        self.__games: dict[str, Callable[[Player, Player], Game]] = games or {
+            "chess": self.__get_chess_game,
+            "connect_four": self.__get_connect_four_game,
+        }
+        self.__logger: Callable[[str], None] = logger or print
+
+    def get_game(self, game: str, player1: Player, player2: Player) -> Game:
         """
         Returns a game with given parameters applied.
 
         Args:
-            socket_service (SocketIOService): Service for handling websocket connection.
             game (str): Game type
-            elo (int, optional): Elo of AI. Defaults to 1350.
+            player1 (Player): Player 1
+            player2 (Player): Player 2
 
         Raises:
             KeyError: Given game is invalid.
@@ -41,25 +35,21 @@ class GameFactory:
             Game: Game with parameters applied.
         """
 
-        return self.__games[game](socket_service, elo, player)
+        return self.__games[game](player1, player2)
 
-    @staticmethod
-    def __get_chess_game(socket_service: SocketService, elo: int, player) -> Game:
+    def __get_chess_game(self, player1: Player, player2: Player) -> Game:
         return Game(
-            socket_service,
-            player,
-            PlayerStockfish(elo),
+            self.__logger,
+            player1,
+            player2,
             ChessJudge(),
         )
 
-    @staticmethod
-    def __get_connect_four_game(
-        socket_service: SocketService, elo: int, player
-    ) -> Game:
+    def __get_connect_four_game(self, player1: Player, player2: Player) -> Game:
         return Game(
-            socket_service,
-            player,
-            PlayerConnectFour(elo),
+            self.__logger,
+            player1,
+            player2,
             ConnectFourJudge(),
         )
 
