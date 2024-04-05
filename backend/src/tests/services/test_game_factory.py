6ed5ac16325fa221935174144d98530a50194e37
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-05 14:29:22 +0300

    refactor(#126): Only pass players to `GameFactory`

--- backend/src/tests/services/test_game_factory.py
+++ backend/src/tests/services/test_game_factory.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import Mock
+from venv import logger
 
 from services.game_factory import GameFactory
 
@@ -14,25 +15,21 @@ class TestGameFactory(unittest.TestCase):
             "connect_four": self.cfour_constructor_mock,
         }
 
-        self.game_factory = GameFactory(games)
-
-        self.socket = Mock()
-        self.playerMock = Mock()
+        self.game_factory = GameFactory(games, logger=Mock())
+        self.player1 = Mock()
+        self.player2 = Mock()
 
     def test_get_game_returns_correct_game(self):
-        self.game_factory.get_game(self.socket, "chess", self.playerMock, 1234)
-        self.game_factory.get_game(self.socket, "connect_four", self.playerMock, 5678)
+        self.game_factory.get_game("chess", self.player1, self.player2)
+        self.game_factory.get_game("connect_four", self.player1, self.player2)
 
         self.chess_constructor_mock.assert_called_once_with(
-            self.socket, 1234, self.playerMock
+            self.player1,
+            self.player2,
         )
 
-        self.cfour_constructor_mock.assert_called_once_with(
-            self.socket, 5678, self.playerMock
-        )
+        self.cfour_constructor_mock.assert_called_once_with(self.player1, self.player2)
 
     def test_get_game_raises_error_if_game_not_exist(self):
         with self.assertRaises(KeyError):
-            self.game_factory.get_game(
-                self.socket, "non-existent game", self.playerMock
-            )
+            self.game_factory.get_game("non-existent game", self.player1, self.player2)
