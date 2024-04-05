91ba34aa876c59f1814da6a79ddc4d1d5df17eff
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-05 15:47:56 +0300

    refactor(#126): Update PlayerFactory typing

--- backend/src/services/player_factory.py
+++ backend/src/services/player_factory.py
@@ -1,22 +1,22 @@
 from collections.abc import Callable
 
-from entities.players.repository_player import RepositoryPlayer
+from entities.players.player import Player
 from entities.players.player_connectfour import PlayerConnectFour
 from entities.players.player_stockfish import PlayerStockfish
 
 
 class PlayerFactory:
-    def __init__(self, players: dict[str, Callable[[int], RepositoryPlayer]]) -> None:
-        self.__games: dict[str, Callable[[int], RepositoryPlayer]] = players or {
+    def __init__(self, players: dict[str, Callable[[int], Player]]) -> None:
+        self.__games: dict[str, Callable[[int], Player]] = players or {
             "chess": self.__get_chess_player,
             "connect_four": self.__get_connect_four_player,
         }
 
-    def get_local_player(self, game: str, difficulty: int) -> RepositoryPlayer:
+    def get_local_player(self, game: str, difficulty: int) -> Player:
         return self.__games[game](difficulty)
 
-    def __get_chess_player(self, difficulty: int) -> RepositoryPlayer:
+    def __get_chess_player(self, difficulty: int) -> Player:
         return PlayerStockfish(difficulty)
 
-    def __get_connect_four_player(self, difficulty: int) -> RepositoryPlayer:
+    def __get_connect_four_player(self, difficulty: int) -> Player:
         return PlayerConnectFour(difficulty)
