26e78d612c2521ba6ca0edfcb30f11d7dbbf6211
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-05 14:37:49 +0300

    feat(#126): Implement PlayerFactory.get_local_player

---
+++ backend/src/services/player_factory.py
@@ -0,0 +1,22 @@
+from collections.abc import Callable
+
+from entities.player import Player
+from entities.player_connectfour import PlayerConnectFour
+from entities.player_stockfish import PlayerStockfish
+
+
+class PlayerFactory:
+    def __init__(self, players: dict[str, Callable[[int], Player]]) -> None:
+        self.__games: dict[str, Callable[[int], Player]] = players or {
+            "chess": self.__get_chess_player,
+            "connect_four": self.__get_connect_four_player,
+        }
+
+    def get_local_player(self, game: str, difficulty: int) -> Player:
+        return self.__games[game](difficulty)
+
+    def __get_chess_player(self, difficulty: int) -> Player:
+        return PlayerStockfish(difficulty)
+
+    def __get_connect_four_player(self, difficulty: int) -> Player:
+        return PlayerConnectFour(difficulty)
