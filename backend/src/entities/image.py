863285d034ae9ffb3cc8307df7d807f3d829c0c7
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-09 12:01:04 +0300

    feat(#124): Add initial Image.__enter__

--- backend/src/entities/image.py
+++ backend/src/entities/image.py
@@ -1,14 +1,34 @@
+from contextlib import AbstractContextManager
 from pathlib import Path
+from types import TracebackType
 from uuid import uuid1
 
-from config import TEMP_DIR
+from spython.main import Client as client
+
+from config import ROOTDIR, TEMP_DIR
 
 
-class Image:
+class Image(AbstractContextManager):
     """
     Class representing a runnable game image without network.
     """
 
     def __init__(self, id_: str | None = None, path: Path | None = None) -> None:
         self.__id: str = id_ or str(uuid1())
-        self.__path: Path = path or TEMP_DIR / "images" / self.__id
+        self.__path: Path = path or TEMP_DIR / f"{self.__id}.sif"
+        self.__client = client
+
+    def __enter__(self) -> "Image":
+        self.__client.build(
+            recipe=str(ROOTDIR / "Singularity"), build_folder=str(self.__path.parent)
+        )
+
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> bool | None:
+        pass
