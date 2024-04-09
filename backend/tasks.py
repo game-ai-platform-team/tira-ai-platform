90975c735f4c71f44821f3c4e898bb4ee974ece5
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-09 11:43:19 +0300

    feat(#124): Add invoke task to generate Singularity definition file

--- backend/tasks.py
+++ backend/tasks.py
@@ -44,3 +44,7 @@ def start(
 @task
 def run_image(ctx):
     ctx.run("poetry run python3 src/run_image.py")
+
+@task
+def convert_recipe(ctx):
+    ctx.run("poetry run spython recipe Dockerfile Singularity")
