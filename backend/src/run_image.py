63edf586b8e7a4cd01b77edd8f0eee9187fd0502
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-09 15:53:41 +0300

    refactor(#124): Pass Repo to PlayerFactory

--- backend/src/run_image.py
+++ backend/src/run_image.py
@@ -1,18 +1,20 @@
 from os import getenv
 
+from git import Repo
+
 from services.game_factory import game_factory
 from services.player_factory import player_factory
 
 if __name__ == "__main__":
     game_type = getenv("GAME")
-    repository_url = getenv("REPOSITORY_URL")
     difficulty = getenv("DIFFICULTY")
+    repository = Repo(getenv("REPOSITORY_PATH"))
 
-    if not (game_type and repository_url and difficulty):
-        raise ValueError("GAME, REPOSITORY_URL, and DIFFICULTY must be set.")
+    if not (game_type and difficulty):
+        raise ValueError("GAME and DIFFICULTY must be set.")
 
     player1 = player_factory.get_local_player(game_type, int(difficulty))
-    player2 = player_factory.get_remote_player(repository_url)
+    player2 = player_factory.get_remote_player(repository)
 
     game = game_factory.get_game(game_type, player1, player2)
 
