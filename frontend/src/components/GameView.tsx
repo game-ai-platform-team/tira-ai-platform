f5d6685d18c4871edc2dc9580580851e7e378e17
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-04-12 11:13:25 +0300

    feat(#120): Fix lint

--- frontend/src/components/GameView.tsx
+++ frontend/src/components/GameView.tsx
@@ -14,6 +14,7 @@ import CFourboard from "./CFourboard.tsx";
 import Gomokuboard from "./Gomokuboard.tsx";
 import Othelloboard from "./Othelloboard.tsx";
 import Home from "./Home.tsx";
+import { Button } from "react-bootstrap";
 
 function GameView() {
     const path = useLocation();
@@ -59,6 +60,21 @@ function GameView() {
                 <Route path="/gomoku" element={<Gomokuboard />} />
                 <Route path="/othello" element={<Othelloboard />} />
             </Routes>
+
+            {ispathempty && (
+                <Button
+                    onClick={() => (
+                        <div className="card">
+                            <Home />
+                        </div>
+                        )}
+                    className="instruction-button"
+                    variant="flat"
+                    size="lg"
+                    aria-label="Instructions"
+                    style={{ color: "black" }}>Instructions
+                </Button>)}
+
             {ispathempty && (
                 <div className="card">
                     <MoveList handleCopyPGN={handleCopyPGN} />
