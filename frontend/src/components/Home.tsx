f8b59e836fd3103046560f3ca0eb09731778aad3
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-04-15 17:13:58 +0300

    test(#132) Add missing GameViewProps from tests

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,9 +1,12 @@
 const Home = () => {
     return (
-        <div style={{fontSize:"2em", color:"#3F3B6C"}}>
-            Do you need help developing your game ai? You've come to the right place!😎
-            <img src="great.png" style={{width:"75%"}} alt="woahhh"/>
-            <marquee>This is a great text for our home page! PLEASE EDIT!!!!</marquee>
+        <div style={{ fontSize: "2em", color: "#3F3B6C" }}>
+            Do you need help developing your game AI? You&apos;ve come to the
+            right place!😎{" "}
+            <img src="great.png" style={{ width: "75%" }} alt="woahhh" />
+            <marquee>
+                <img src="great.png" style={{ width: "75%" }} alt="woahhh" />
+            </marquee>
         </div>
     );
 };
