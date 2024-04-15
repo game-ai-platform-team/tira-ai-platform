f291bb499f241ef48327adc56154c9d2feb964a1
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-04-15 15:57:29 +0300

    test(#132) Add tests to Cfourreducer
    Co-authored-by: Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>

---
+++ frontend/src/tests/reducers/board/connectFourBoardReducer.test.ts
@@ -0,0 +1,46 @@
+import store from "../../../store";
+import { newConnectFourBoard } from "../../../reducers/board/connectFourBoardReducer";
+import resetStateReducer from "../../../reducers/resetReducer";
+
+describe("ConnectFourBoardReducer", () => {
+    beforeEach(() => {
+        store.dispatch(resetStateReducer());
+    });
+
+    test("changes board", () => {
+        store.dispatch(
+            newConnectFourBoard({ move: "3", logs: "", time: 100, evaluation: 0 }),
+        );
+        expect(store.getState().boards.connectFourBoards).toHaveLength(1);
+        expect(store.getState().boards.connectFourBoards[0][0]).toEqual(
+            3,
+        );
+    });
+
+    test("changes board multiple times", () => {
+        store.dispatch(
+            newConnectFourBoard({
+                move: "1",
+                logs: "something",
+                time: 123,
+                evaluation: -1,
+            }),
+        );
+        store.dispatch(
+            newConnectFourBoard({
+                move: "6",
+                logs: "something",
+                time: 123,
+                evaluation: -1,
+            }),
+        );
+
+        expect(store.getState().boards.connectFourBoards).toHaveLength(2);
+        expect(store.getState().boards.connectFourBoards[0][0]).toEqual(
+            1,
+        );
+        expect(store.getState().boards.connectFourBoards[1][1]).toEqual(
+            6,
+        );
+    });
+});
