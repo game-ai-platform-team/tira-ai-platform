7f77295bb72ca63c319c7703e7e02442b58cad89
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-15 13:16:33 +0300

    ci: Allow running invoke tasks in subdirectories

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,37 +1,41 @@
+from pathlib import Path
+
 from invoke.tasks import task
+
+SOURCE_DIR = Path(__file__).parent / "src"
 
 
 @task
 def format(ctx):
-    ctx.run("poetry run black . --exclude temp")
-    ctx.run("poetry run isort . --profile=black")
+    ctx.run(f"poetry run black {SOURCE_DIR} --exclude temp")
+    ctx.run(f"poetry run isort {SOURCE_DIR} --profile=black")
 
 
 @task
 def test(ctx):
-    ctx.run("poetry run pytest src/tests --color=yes -n auto", pty=True)
+    ctx.run(f"poetry run pytest {SOURCE_DIR} --color=yes -n auto", pty=True)
 
 
 @task
 def coverage_html(ctx):
-    ctx.run("pytest --cov src --cov-report html -n auto", pty=True)
+    ctx.run(f"pytest --cov {SOURCE_DIR} --cov-report html -n auto", pty=True)
 
 
 @task
 def coverage_xml(ctx):
-    ctx.run("pytest --cov src --cov-report xml -n auto", pty=True)
+    ctx.run(f"pytest --cov {SOURCE_DIR} --cov-report xml -n auto", pty=True)
 
 
 @task
 def lint(ctx):
-    ctx.run("poetry run pylint src -j 0")
+    ctx.run(f"poetry run pylint {SOURCE_DIR} -j 0")
 
 
 @task
 def dev(
     ctx,
 ):
-    ctx.run("poetry run python3 src/app.py")
+    ctx.run(f"poetry run python3 {SOURCE_DIR}/app.py")
 
 
 @task
@@ -43,4 +47,4 @@ def start(
 
 @task
 def run_image(ctx):
-    ctx.run("poetry run python3 src/run_image.py")
+    ctx.run(f"poetry run python3 {SOURCE_DIR}/run_image.py")
