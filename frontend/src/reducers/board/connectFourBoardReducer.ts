fb968120e5876de8e752a82398c2a6360da4744e
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-04-15 15:56:56 +0300

    Fix(#132): Ffix for cfourreducer
    Co-authored-by: Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>

--- frontend/src/reducers/board/connectFourBoardReducer.ts
+++ frontend/src/reducers/board/connectFourBoardReducer.ts
@@ -7,7 +7,7 @@ const connectFourSlice = createSlice({
     initialState: new Array<Array<number>>(),
     reducers: {
         newConnectFourBoard(state, action: PayloadAction<MoveStatistics>) {
-            const move = toNumber(action.payload);
+            const move = toNumber(action.payload.move);
             const previousBoard = state[state.length - 1] || [];
 
             state.push(previousBoard.concat(move));
