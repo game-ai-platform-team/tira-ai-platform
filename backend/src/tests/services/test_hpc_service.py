ee38171d8aa861c0859a33ec0cd0c80e1db59e9e
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-15 13:56:44 +0300

    test(#124): Add tests for HPCService

---
+++ backend/src/tests/services/test_hpc_service.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+from unittest import TestCase
+from unittest.mock import MagicMock, Mock, patch
+
+from config import TEMP_DIR
+from services.hpc_service import HPCService
+
+
+class TestHPCService(TestCase):
+    def setUp(self) -> None:
+        self.connection = MagicMock()
+        self.__id = "id1234"
+        self.hpc_service = HPCService(self.connection, self.__id)
+
+    def test_output_path(self):
+        self.assertEqual(self.hpc_service.output_path, Path(f"result-{self.__id}.txt"))
+
+    def test_batch_path(self):
+        self.assertEqual(
+            self.hpc_service.batch_path, TEMP_DIR / f"batch-{self.__id}.sh"
+        )
+
+    def test_enter_starts_connection(self):
+        with self.hpc_service:
+            self.connection.__enter__.assert_called_once()
+
+    def test_enter_creates_output_file(self):
+        with self.hpc_service as hpc:
+            self.connection.execute.assert_called_once_with(f"touch {hpc.output_path}")
+
+    def test_exit_closes_connection(self):
+        with self.hpc_service:
+            pass
+
+        self.connection.__exit__.assert_called_once()
+
+    def test_submit_sends_image(self):
+        image = Mock()
+
+        with self.hpc_service as hpc:
+            hpc.submit(image)
+
+        self.connection.send_file.assert_any_call(image)
+
+    @patch.object(
+        HPCService, "_HPCService__create_script", lambda x, y: Path("batch_path")
+    )
+    def test_submit_sends_batch_file(self):
+        with self.hpc_service as hpc:
+            hpc.submit(Mock())
+
+        self.connection.send_file.assert_any_call(Path("batch_path"))
+
+    def test_submit_writes_batch_file(self):
+        pass
+
+    def read_output_remembers_position(self):
+        pass
