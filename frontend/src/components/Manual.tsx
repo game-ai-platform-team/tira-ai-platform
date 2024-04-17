485d27d8105719430f10ada04715f71d26acca56
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-04-17 14:15:26 +0300

    docs: Comments to Manual

--- frontend/src/components/Manual.tsx
+++ frontend/src/components/Manual.tsx
@@ -6,12 +6,21 @@ import connect_four from "../../../docs/user_manual/connect_four.md";
 interface manualProps {
     game: string;
 }
-
+/**
+ * Draws a markdown file containing a manual.
+ * Uses ReactMarkdown to render .md files
+ * The manual shown depends on the relevant game
+ *
+ * @param String matching the desired manual. 
+ * Unless parameter is "chess" or "connect_four" resorts to the general manual  
+ * 
+ * @returns A manual matching the relevant game or a general manual if no game is relevant
+ */
 const GeneralManual = (props: manualProps) => {
     const currentManual =
         props.game === "chess"
             ? chess
-            : props.game === "cfour"
+            : props.game === "connect_four"
               ? connect_four
               : manual;
     return (
