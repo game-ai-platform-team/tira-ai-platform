48ebe635690a34edf6046ecdea6bf0473a4e78eb
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-04-17 14:26:50 +0300

    docs: Comments to Notification

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -1,3 +1,11 @@
+/**
+ * React component for displaying notifications using Bootstrap Toast.
+ * 
+ * This component utilizes the Toast component from react-bootstrap/Toast
+ * and interacts with the toastReducer for managing notification state.
+ * 
+ * @returns {JSX.Element} The Notification component JSX.
+ */
 import Toast from "react-bootstrap/Toast";
 import { useAppDispatch, useAppSelector } from "../hook";
 import { setToast } from "../reducers/toastReducer";
