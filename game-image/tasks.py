2cfe1ac1382c28f3a07dfce825572a8f03b66174
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-18 12:45:43 +0300

    ci(#124): Configure CI/CD for game-image

--- game-image/tasks.py
+++ game-image/tasks.py
@@ -8,3 +8,29 @@ SOURCE_DIR = Path(__file__).parent / "src"
 @task
 def run(ctx):
     ctx.run(f"poetry run python3 {SOURCE_DIR}/run_image.py")
+
+
+@task
+def format(ctx):
+    ctx.run(f"poetry run black {SOURCE_DIR} --exclude temp")
+    ctx.run(f"poetry run isort {SOURCE_DIR} --profile=black")
+
+
+@task
+def test(ctx):
+    ctx.run(f"poetry run pytest {SOURCE_DIR} --color=yes -n auto", pty=True)
+
+
+@task
+def coverage_html(ctx):
+    ctx.run(f"pytest {SOURCE_DIR} --cov  --cov-report html -n auto", pty=True)
+
+
+@task
+def coverage_xml(ctx):
+    ctx.run(f"pytest {SOURCE_DIR} --cov --cov-report xml -n auto", pty=True)
+
+
+@task
+def lint(ctx):
+    ctx.run(f"poetry run pylint {SOURCE_DIR} -j 0")
