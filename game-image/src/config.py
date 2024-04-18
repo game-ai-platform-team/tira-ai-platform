b0b6d7fbc51c8d1fc7179e252825c47c4e138316
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-18 13:16:06 +0300

    chore: Add BINARY_DIR

--- game-image/src/config.py
+++ game-image/src/config.py
@@ -2,4 +2,8 @@ from os import getenv
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent
+
+BINARY_DIR = ROOT_DIR / "bin"
+BINARY_DIR.mkdir(exist_ok=True)
+
 DEFAULT_CHESS_TIMEOUT = float(getenv("CHESS_TIMEOUT") or 5)
