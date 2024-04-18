a6b5aa2c5f3ea0a65cd8acd29b2cc5f9d2cdacaa
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-18 17:24:01 +0300

    refactor: Rename ROOT_DIR to BACKEND_DIR

--- backend/src/config.py
+++ backend/src/config.py
@@ -3,18 +3,18 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-ROOT_DIR = Path(__file__).parent.parent
+BACKEND_DIR = Path(__file__).parent.parent
 
 try:
-    load_dotenv(dotenv_path=ROOT_DIR / ".env")
-    load_dotenv(dotenv_path=ROOT_DIR / ".env.secret")
+    load_dotenv(dotenv_path=BACKEND_DIR / ".env")
+    load_dotenv(dotenv_path=BACKEND_DIR / ".env.secret")
 except FileNotFoundError:
     pass
 
-TEMP_DIR = ROOT_DIR / "temp"
+TEMP_DIR = BACKEND_DIR / "temp"
 TEMP_DIR.mkdir(exist_ok=True)
 
-IMAGE_DIR = ROOT_DIR.parent / "game-image"
+IMAGE_DIR = BACKEND_DIR.parent / "game-image"
 
 DEFAULT_CHESS_TIMEOUT = float(getenv("CHESS_TIMEOUT") or 5)
 
@@ -28,6 +28,6 @@ OIDC_REDIRECT_PATH = getenv("OIDC_REDIRECT_PATH") or "https://localhost:5000"
 HPC_LOGIN_NODE = getenv("HPC_LOGIN_NODE")
 HPC_USERNAME = getenv("HPC_USERNAME")
 HPC_SSH_PRIVATE_KEY_PATH = (
-    getenv("HPC_SSH_PRIVATE_KEY_LOCATION") or ROOT_DIR / "hpc_private_key"
+    getenv("HPC_SSH_PRIVATE_KEY_LOCATION") or BACKEND_DIR / "hpc_private_key"
 )
 HPC_GAME_IMAGE_PATH = getenv("HPC_GAME_IMAGE_PATH")
