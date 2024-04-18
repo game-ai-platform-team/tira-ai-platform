84e8a6db61d248fa562fa8e89231d52dcf7ff403
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-18 14:18:21 +0300

    Revert "chore: Remove BINARY_DIR"
    This reverts commit b6afcb53dd68ecc1d18b2807317c744e1dcb7483.

--- game-image/src/config.py
+++ game-image/src/config.py
@@ -3,5 +3,8 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent
 
+BINARY_DIR = ROOT_DIR / "bin"
+BINARY_DIR.mkdir(exist_ok=True)
+
 STOCKFISH_PATH = "stockfish"
 DEFAULT_CHESS_TIMEOUT = float(getenv("CHESS_TIMEOUT") or 5)
