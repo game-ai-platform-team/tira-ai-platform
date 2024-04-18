561c27c5818a22c41639e3b449ac30566c873bdb
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-18 15:12:42 +0300

    feat(#134): Install Stockfish for chess game

--- game-image/src/config.py
+++ game-image/src/config.py
@@ -6,5 +6,6 @@ ROOT_DIR = Path(__file__).parent.parent
 BINARY_DIR = ROOT_DIR / "bin"
 BINARY_DIR.mkdir(exist_ok=True)
 
-STOCKFISH_PATH = "stockfish"
+STOCKFISH_PATH = BINARY_DIR / "stockfish"
+
 DEFAULT_CHESS_TIMEOUT = float(getenv("CHESS_TIMEOUT") or 5)
