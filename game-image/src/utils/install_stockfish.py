b124947c393397933b9daf98e074615df980e05c
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-18 15:12:03 +0300

    feat(#134): Add script to install Stockfish if missing

---
+++ game-image/src/utils/install_stockfish.py
@@ -0,0 +1,29 @@
+import tarfile
+from io import BytesIO
+from pathlib import Path
+from shutil import copy
+from tempfile import TemporaryDirectory
+from urllib3 import request
+
+
+def install_stockfish(path: Path) -> None:
+    """
+    Installs stockfish binary if missing.
+
+    Args:
+        path (Path): Path to install stockfish binary
+    """
+
+    if path.exists():
+        return
+
+    stockfish_url = "https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-ubuntu-x86-64-avx2.tar"
+
+    response = request("GET", stockfish_url)
+
+    with (
+        TemporaryDirectory() as temp_dir,
+        tarfile.open(fileobj=BytesIO(response.data)) as file,
+    ):
+        file.extract("stockfish/stockfish-ubuntu-x86-64-avx2", temp_dir)
+        copy(temp_dir / Path("stockfish/stockfish-ubuntu-x86-64-avx2"), path)
