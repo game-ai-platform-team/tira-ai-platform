bf50289bfb703e002ffed1f5ccbb6427e76cf080
Hipsterisiili <rasmus.marttila@hotmail.com>
2024-04-19 17:05:31 +0300

    feat(#133): Fix SubmitForm comments

--- frontend/src/components/SubmitForm.tsx
+++ frontend/src/components/SubmitForm.tsx
@@ -10,9 +10,9 @@ import { setToast } from "../reducers/toastReducer";
 import { useLocation } from "react-router-dom";
 
 /**
- * Renders a form for submitting game configurations including Elo rating or difficulty level,
- * number of games to play, and repository URL for AI code. Provides options for starting a new game,
- * resetting the current game state, and displays notifications for game outcomes and actions.
+ * Renders a form for submitting game configurations including Elo rating or difficulty level
+ * and repository URL for AI code. Provides options for starting a new gameand a button for 
+ * resetting the current game state.
  *
  * @returns {JSX.Element} JSX element containing the rendered submit form.
  */
