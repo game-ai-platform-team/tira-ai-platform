a45fac28084449839dc7595e1950173d47226a06
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2024-04-19 17:39:01 +0300

    feat(#136): add a textarea box for logs

--- frontend/src/components/LogBox.tsx
+++ frontend/src/components/LogBox.tsx
@@ -1,4 +1,5 @@
 import { useAppSelector } from "../hook.ts";
+import "../scss/LogBox.scss";
 /**
  * Draws logs printed by the AI
  *
@@ -8,9 +9,11 @@ export function LogBox() {
     const logs = useAppSelector((state) => state.allLog);
 
     return (
-        <>
+        <div id="log-box">
             <h2 className="card-header">All logs</h2>
-            <code style={{ whiteSpace: "pre" }}>{logs}</code>
-        </>
+            <div>
+                <textarea value={logs} readOnly />
+            </div>
+        </div>
     );
 }
