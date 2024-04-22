4337c9a089283e5dfd1b8ef35afbb19465f5f6d0
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2024-04-22 16:47:58 +0300

    feat(#136): adjust log box height
    Co-authored-by: skeltal2 <skeltal2@users.noreply.github.com>

--- frontend/src/components/LogBox.tsx
+++ frontend/src/components/LogBox.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { useAppSelector } from "../hook.ts";
 import "../scss/LogBox.scss";
 /**
@@ -7,12 +8,24 @@ import "../scss/LogBox.scss";
  */
 export function LogBox() {
     const logs = useAppSelector((state) => state.logs);
+    const textareaRef = useRef(null);
+
+    useEffect(() => {
+        if (textareaRef.current) {    
+            textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px';
+            textareaRef.current.scrollTop = textareaRef.current.scrollHeight
+    }
+
+    }, [logs]);
+    
+    
+
 
     return (
         <div id="log-box">
             <h2 className="card-header">All logs</h2>
             <div>
-                <textarea value={logs} readOnly />
+                <textarea ref={textareaRef} value={logs} readOnly />
             </div>
         </div>
     );
