28b810507c3e3192781d66416169a872ed5f0ab3
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2024-04-23 21:22:52 +0300

    feat(#129): add a description to the home page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -6,11 +6,47 @@
  */
 const Home = (): JSX.Element => {
     return (
-        <div style={{ fontSize: "2em", color: "#3F3B6C" }}>
-            Do you need help developing your game AI? You&apos;ve come to the
-            right place!😎{" "}
-            <img src="great.png" style={{ width: "75%" }} alt="woahhh" />
-            <img src="great.png" style={{ width: "75%" }} alt="woahhh" />
+        <div id="manual-view">
+            <div
+                className="card container"
+                style={{ fontSize: "1.5em", color: "#3F3B6C" }}
+            >
+                <h1 style={{ marginBottom: "20px" }}>Tira-AI-Platform</h1>
+                <p>
+                    Welcome to Tira-AI-Platform, your go-to-solution for
+                    students working on their AI and algorithms project who are
+                    developing an AI for a game. Our platform offers you a tool
+                    to visualize and test your progress, with support currently
+                    available for chess and Connect Four.
+                </p>
+                <p>
+                    How does it work? It's simple: submit your project via a
+                    GitHub link and test your AI against our platform's built-in
+                    AI. This process lets you refine your algorithm and see your
+                    AI strategies in action.
+                </p>
+                <p>
+                    But that's not all! Tira-AI-Platform goes beyond just
+                    testing. You can visualize your gameplay directly on the
+                    platform, gaining valuable insights through stats, logs, and
+                    a record of all moves made during the game.
+                </p>{" "}
+                <p>
+                    {" "}
+                    For instructions, check out the user manuals located at the
+                    top of the page under instructions.
+                </p>
+            </div>
+            <p></p>
+            <p></p>
+            <div className="card container">
+                <h2>Good luck with your AI!</h2>
+                <img
+                    src="great.png"
+                    style={{ alignSelf: "center", width: "75%" }}
+                    alt="woahhh"
+                />
+            </div>
         </div>
     );
 };
