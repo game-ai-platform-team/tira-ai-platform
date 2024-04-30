076b6f099e70fbde3b665cad62658ae9940ed0ea
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-04-30 14:35:00 +0300

    refactor: Add type hints to tasks

--- backend/tasks.py
+++ backend/tasks.py
@@ -8,41 +8,37 @@ SOURCE_DIR = ROOT_DIR / "src"
 
 
 @task
-def format(ctx):
+def format(ctx: Context) -> None:
     ctx.run(f"poetry run black {ROOT_DIR} --exclude temp")
     ctx.run(f"poetry run isort {ROOT_DIR} --profile=black")
 
 
 @task
-def test(ctx):
+def test(ctx: Context) -> None:
     ctx.run(f"poetry run pytest {SOURCE_DIR} --color=yes -n auto", pty=True)
 
 
 @task
-def coverage_html(ctx):
+def coverage_html(ctx: Context) -> None:
     ctx.run(f"pytest {SOURCE_DIR} --cov  --cov-report html -n auto", pty=True)
 
 
 @task
-def coverage_xml(ctx):
+def coverage_xml(ctx: Context) -> None:
     ctx.run(f"pytest {SOURCE_DIR} --cov --cov-report xml -n auto", pty=True)
 
 
 @task
-def lint(ctx):
+def lint(ctx: Context) -> None:
     ctx.run(f"poetry run pylint {SOURCE_DIR} -j 0")
 
 
 @task
-def dev(
-    ctx: Context,
-):
+def dev(ctx: Context) -> None:
     with ctx.prefix("export MODE=development"):
         start(ctx)
 
 
 @task
-def start(
-    ctx,
-):
+def start(ctx: Context) -> None:
     ctx.run("poetry run dotenv --file .env.${MODE:=production} run uwsgi")
